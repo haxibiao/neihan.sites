f69e9855f81cd180b3a524c14a8ea0a05ce8b6e9
pengchong <pengchong@haxibiao.com>
2017-06-27 08:29:36 +0800

    add env.prod, enable run push ...

---
+++ Envoy.blade.php
@@ -0,0 +1,46 @@
+@include('config/envoy.php')
+
+@servers(['local' => 'localhost','hk001' => $hk001, 'hk002' => $hk002, 'hk002' => $hk002])
+
+@macro('push')
+push
+web_pull
+@endmacro
+
+@macro('seed')
+push
+web_seed
+@endmacro
+
+@macro('update')
+push
+web_update
+@endmacro
+
+@task('push', ['on' => 'local'])
+hostname
+cd {{ $www }}
+git push
+@endtask
+
+@task('web_pull', ['on' => ['hk002'], 'parallel' => true])
+cd {{ $www }}
+{{ $refresh_env_config }}
+{{ $cache_clear }}
+@endtask
+
+@task('web_seed', ['on' => ['hk002'], 'parallel' => true])
+cd {{ $www }}
+{{ $refresh_env_config }}
+{{ $run_migrate }}
+{{ $cache_clear }}
+@endtask
+
+@task('web_update', ['on' => ['hk002'], 'parallel' => true])
+cd {{ $www }}
+{{ $refresh_env_config }}
+{{ $clear_bootstrap_cache }}
+{{ $run_composer }}
+{{ $run_migrate }}
+{{ $cache_clear }}
+@endtask
---
+++ config/envoy.php
@@ -0,0 +1,39 @@
+<?php
+
+$git = 'root@hk002:/data/www/dongdianyi.com';
+$www = '/data/www/dongdianyi.com';
+
+$hk001 = 'root@hk001';
+$hk002 = 'root@hk002';
+$hk003 = 'root@hk003';
+
+$git_pull_force = <<<EOT
+chmod -R 777 .
+chown -R www:www .
+git config core.filemode false
+git checkout .
+git pull
+EOT;
+
+$clear_bootstrap_cache = <<<EOT
+rm -rf bootstrap/cache/*.php
+EOT;
+
+$refresh_env_config = <<<EOT
+/bin/cp -rf .env.prod .env
+EOT;
+
+$run_composer = <<<EOT
+composer install
+composer dump-autoload
+EOT;
+
+$run_migrate = <<<EOT
+php artisan migrate --seed --force
+EOT;
+
+$cache_clear = <<<EOT
+php artisan cache:clear
+EOT;
+
+?>
