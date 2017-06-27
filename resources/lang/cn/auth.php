953b53e9f56e5bce23ac366e39412883f54e4441
ivan-old <ivan@ivan-old>
2017-06-27 11:39:02 +0800

    汉化注册页面...

---
+++ resources/lang/cn/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+	/*
+		    |--------------------------------------------------------------------------
+		    | Authentication Language Lines
+		    |--------------------------------------------------------------------------
+		    |
+		    | The following language lines are used during authentication for various
+		    | messages that we need to display to the user. You are free to modify
+		    | these language lines according to your application's requirements.
+		    |
+	*/
+
+	'failed' => '这些登录信息不存在.',
+	'throttle' => '失败次数过多. 请等待 :seconds 秒后重试.',
+
+];
---
+++ resources/lang/cn/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+	/*
+		    |--------------------------------------------------------------------------
+		    | Password Reset Language Lines
+		    |--------------------------------------------------------------------------
+		    |
+		    | The following language lines are the default lines which match reasons
+		    | that are given by the password broker for a password update attempt
+		    | has failed, such as for an invalid token or invalid new password.
+		    |
+	*/
+
+	'password' => '密码最少需要包含6个字符.',
+	'reset' => '您的密码已重置!',
+	'sent' => '我们已经把密码重置地址发送邮件给您!',
+	'token' => '这个密码重置令牌已经过期.',
+	'user' => "这个邮件地址不存在对应的账户.",
+
+];
