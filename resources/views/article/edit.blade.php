a10e7c9c32ab8fd1d51f463476c7f39cfa93971c
ivan-old <ivan@ivan-old>
2017-07-04 07:36:52 +0800

    修复编辑文章的时候，也能插图片，完成图片关联

--- resources/views/article/edit.blade.php
+++ resources/views/article/edit.blade.php
@@ -65,57 +65,12 @@
         </div>
     
         <div class="btn-group pull-right">
+            <input type="hidden" name="image_url">
             {!! Form::reset("重置", ['class' => 'btn btn-warning']) !!}
             {!! Form::submit("保存", ['class' => 'btn btn-success']) !!}
         </div>
     
     {!! Form::close() !!}
-
-    {{-- <form action="/article" method="POST" role="form" enctype="multipart/form-data" id='article_form'>
-      {{ csrf_field() }}
-      <legend>编辑文章</legend>
-
-      <div class="form-group">
-        <label>标题</label>
-        <input type="text" class="form-control" placeholder="标题" name="title" required="true">
-      </div>
-      <div class="row">        
-        <div class="col-md-6">
-          <div class="form-group{{ $errors->has('category_id') ? ' has-error' : '' }}">
-              {!! Form::label('category_id', '分类') !!}
-              {!! Form::select('category_id', $categories, null, ['id' => 'category_id', 'class' => 'form-control', 'required' => 'required']) !!}
-              <small class="text-danger">{{ $errors->first('category_id') }}</small>
-          </div>
-        </div>
-        <div class="col-md-6">
-          <div class="form-group">
-            <label>作者</label>
-            <input type="hidden" name="user_id" value="{{ Auth::user()->id }}">
-            <input type="text" class="form-control" placeholder="作者" name="author" value="{{ Auth::user()->name }}" readonly="true">
-          </div>
-        </div>
-      </div>
-      <div class="form-group">
-        <label>关键词</label>
-        <input type="text" class="form-control" placeholder="关键词" name="keywords" required="true">
-      </div>
-      <div class="form-group">
-        <label>简介</label>
-        <input type="text" class="form-control" placeholder="简介" name="description" required="true">
-      </div>
-      <div class="form-group">
-        <label>正文</label>
-        <textarea class="hidden"  name="body" required="true"></textarea>
-        <div class="editable"></div>
-      </div>
-
-      <div class="form-group">
-        <input type="hidden" name="image_url">
-        <button type="submit" class="btn btn-danger">提交</button>
-      </div>
-
-      
-    </form> --}}
   </div>
   <div class="col-md-5">
       @include('article.parts.image_upload_ui')
