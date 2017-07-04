62c209fb7085256e56596125941e29207bee557f
ivan-old <ivan@ivan-old>
2017-07-04 13:19:53 +0800

    fix use save profile error

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,7 +10,7 @@ class UserController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth.admin', ['except' => ['index', 'show']]);
+        // $this->middleware('auth.admin', ['except' => ['index', 'show']]);
     }
     /**
      * Display a listing of the resource.
