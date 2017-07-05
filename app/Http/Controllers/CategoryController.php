dd91af349a69d5d4736039910fb17b3c492d9b9d
ivan-old <ivan@ivan-old>
2017-07-05 15:42:03 +0800

    修复分类目录页需要登录的问题

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -12,7 +12,7 @@ class CategoryController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('auth', ['except' => 'name_en']);
     }
 
     /**
