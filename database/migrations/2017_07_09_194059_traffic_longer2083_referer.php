7a3d11c588f5a1cd2d5c25f70c72ac2078dedcce
boori3 <boor@ivani3>
2017-07-09 19:53:07 +0800

    fix referer longer2083

--- database/migrations/2017_07_09_194059_traffic_longer2083_referer.php
+++ database/migrations/2017_07_09_194059_traffic_longer2083_referer.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class TrafficLongerReferer extends Migration
+class TrafficLonger2083Referer extends Migration
 {
     /**
      * Run the migrations.
