c231b8bcd56e8f791561611f3dc61b9d985a3bd0
ivan-old <ivan@ivan-old>
2017-07-14 08:50:01 +0800

    API: 提供用户的基础数据,可以按用户名查询用户

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\User;
+use App\Article;
+use App\Traffic;
+use Carbon\Carbon;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +19,27 @@ use Illuminate\Http\Request;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+//获取用户详细资料
+Route::get('/user/{id}', function(Request $request, $id){
+	$user = User::findOrFail($id);
+	$data['user'] = $user;
+
+	$data['articles_count'] = Article::where('user_id', $user->id)->count();
+	$data['traffic_count'] = Traffic::where('user_id', $user->id)->count();
+
+	$data['articles_count_yesterday'] = Article::where('user_id', $user->id)->where('date', Carbon::now()->subDay(1)->toDateString())->count();
+	$data['traffic_count_yesterday'] = Traffic::where('user_id', $user->id)->where('date', Carbon::now()->subDay(1)->toDateString())->count();
+
+	$data['articles_count_today'] = Article::where('user_id', $user->id)->where('date', Carbon::now()->toDateString())->count();
+	$data['traffic_count_today'] = Traffic::where('user_id', $user->id)->where('date', Carbon::now()->toDateString())->count();
+
+	return $data;
+});
+
+//get user id by user name
+Route::get('/user/by-name/{name}', function(Request $request, $name){
+	$user = User::where('name',$name)->first();
+	return $user;
 });
