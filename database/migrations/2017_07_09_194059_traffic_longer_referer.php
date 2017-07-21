8f6bf565336304204b1eac6aa10fb23585a9971a
ivan-old <ivan@ivan-old>
2017-07-21 09:35:26 +0800

    add ainicheng.com

---
+++ database/migrations/2017_07_09_194059_traffic_longer_referer.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class TrafficLongerReferer extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('traffic', function (Blueprint $table) {
+           $table->string('referer', 255)->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('traffic', function (Blueprint $table) {
+            //
+        });
+    }
+}
