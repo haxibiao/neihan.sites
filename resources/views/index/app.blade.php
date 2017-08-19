c2e3dfae9eb05a31e39d4f7f71695f99aae4a0ff
ivan-amd <ivan@ubunt.amd>
2017-08-19 14:46:46 +0800

    更新app下载链接

--- resources/views/index/app.blade.php
+++ resources/views/index/app.blade.php
@@ -11,20 +11,20 @@
    		<h3 class="panel-title">{{ config('app.name') }}App</h3>
    	</div>
    	<div class="panel-body">
-   		<h1>
-            关于{{ config('app.name') }}App的更新
-        </h1>
+   		<h2>
+            关于{{ config('app.name') }}App
+        </h2>
         <p>
             我们目前还在紧张的开发和测试APP, 请过几天来关注这里，随时会发布正式版 ...
         </p>
         <p>
-            <a class="btn btn-danger btn-lg" href="http://haxibiao-1251052432.cosgz.myqcloud.com/dongdianyi.apk" target="_blank">
-                下载{{ config('app.name') }}安卓版(内部测试App)
+            <a class="btn btn-success" href="http://haxibiao-1251052432.cosgz.myqcloud.com/{{ str_replace(".com", "", get_domain()) }}.apk" target="_blank">
+                下载安卓版(内测App)
             </a>
         <hr>
-            <span class="alert alert-info">
-                微信用户注意点右上角的 ...　然后选择浏览器打开，就可以下载成功.
-            </span>
+            <p class="small">
+              微信用户注意点右上角的 ...　然后选择浏览器打开，就可以下载成功.
+            </p>
         </p>
    	</div>
    </div>
