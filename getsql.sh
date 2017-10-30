8f497f5b96d14be9ce9cd348b4c4a37964a38de9
ivan <ivan@big>
2017-10-30 23:48:48 +0800

    更新　bash getsql.sh 支持 server 参数

--- getsql.sh
+++ getsql.sh
@@ -2,6 +2,11 @@
 
 source ~/.bash_aliases
 
+if [ -z $1 ]; then
+	echo '请指定数据库服务器，比如　bash getsql.sh hk001'
+	exit
+fi
+
 if [ ! -f /data/build/ssh/id_rsa ]; then
 	echo 'set up ssh keys ...'
 	mkdir -p /data/build/ssh
@@ -10,23 +15,22 @@ if [ ! -f /data/build/ssh/id_rsa ]; then
 	[ ! -f id_rsa.pub ] && wget https://haxibiao.com/work/id_rsa.pub
 fi
 
-if [ -z $1 ]; then
-	cd /data/www/ainicheng.com
-	php artisan get:sql
+if [ -z $2 ]; then
+	php artisan get:sql --server=$1
 fi
 
 
 [ ! -d /data/sqlfiles ] && mkdir /data/sqlfiles
 
-rsync -P --rsh=ssh root@ainicheng.com:/data/sqlfiles/ainicheng.sql.zip /data/sqlfiles
+rsync -P --rsh=ssh root@$1:/data/sqlfiles/dianmoge.sql.zip /data/sqlfiles
 
 cd /data/sqlfiles
 echo '解压...'
-unzip -o ainicheng.sql.zip
+unzip -o dianmoge.sql.zip
 echo '恢复...'
 
 #注意，请在 ~/.bash_aliases 里增加一个　　alias sql='mysql -uroot -plocaldb001'
-#mysql -uroot -plocaldb001 ainicheng<ainicheng.sql
-sql ainicheng<ainicheng.sql
+#mysql -uroot -plocaldb001 dianmoge<dianmoge.sql
+sql dianmoge<dianmoge.sql
 
 echo '数据库恢复本地完成...'
