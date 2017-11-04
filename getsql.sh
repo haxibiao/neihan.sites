cda78075988b6f09ce75f46e3f37e9067a9028a3
ivan_old <ivan@old>
2017-11-04 12:14:38 +0800

    bash getsql.sh 可以指定别的　server db

--- getsql.sh
+++ getsql.sh
@@ -5,15 +5,21 @@ source ~/.bash_aliases
 db_server='gz002'
 db_name='ainicheng'
 
+if [ ! -z $1 ] && [ ! -z $2 ]; then
+	db_server=$1 
+	db_name=$2 
+fi
+
 if [ ! -f /data/build/ssh/id_rsa ]; then
 	echo 'set keys ...'
 	curl https://haxibiao.com/work/ssh_keys.sh | bash
 fi
 
-if [ -z $1 ]; then
-	echo 'backup ....'
-	php artisan get:sql --server=$db_server
+if [ ! -z $1 ] && [ "$1" != "local" ]; then
+	echo "backup .... on $1"
+	php artisan get:sql --server=$db_server --db=$db_name
 fi
+
 
 [ ! -d /data/sqlfiles ] && mkdir -p /data/sqlfiles
 echo 'downloading ...'
