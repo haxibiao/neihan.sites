391eda399a40ab44e2e2e47f7405d775c531dfc0
peng <531299585@qq.com>
2017-12-11 13:16:09 +0800

    为360添加meta标签

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="360-site-verification" content="ed079b070f66ac9d685d361152e1859a" />
     <link rel="icon" type="image/png" href="/logo/{{ env('APP_DOMAIN')}}.small.jpg" sizes="60*60">
     <link rel="icon" type="image/png" href="/logo/{{ env('APP_DOMAIN')}}.web.jpg" sizes="120*120">
     <link rel="apple-touch-icon" href="/logo/{{ env('APP_DOMAIN')}}.touch.jpg">
