cb798bb35c6c7b36e9a26d9bba434a3e1b934401
peng <531299585@qq.com>
2017-12-27 10:11:27 +0800

    修改部分测试规则

--- tests/Feature/ApiTest.php
+++ tests/Feature/ApiTest.php
@@ -36,8 +36,8 @@ class ApiTest extends TestCase
     	$response=$this->post("/api/like/$article->id/article",[
                 'api_token'=>$user->api_token,
         ]);
-
         $response->assertStatus(200);
+        $response->assertSeeText('liked');
     }
 
     public function testUpdateUserInfo(){
