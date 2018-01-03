1e0e770412c6507e9f5b34e72dc6a6d2f3b474bc
peng <531299585@qq.com>
2018-01-03 09:53:25 +0800

    修改创建match的算法

--- app/Http/Controllers/MatchController.php
+++ app/Http/Controllers/MatchController.php
@@ -54,10 +54,64 @@ class MatchController extends Controller
         $teams      = $compare->teams;
 
         //获取当前赛季的分组情况
-        $teams_count= $teams->count();
-        if($teams_count <= 8){
-            $teams_a=$teams->where('group','A');
-            dd($teams_a);
+        $teams_count = $teams->count();
+        if ($teams_count <= 8) {
+            $teams_a     = $teams->where('group', 'A');
+            $teams_b     = $teams->where('group', 'B');
+            $teams_human = $teams_a->count();
+            $this->create_match($teams_a,$teams_b, $teams_human,$compare_id);
+        } else {
+            $team_group = $this->team_group($teams_count, $teams);
+
+        }
+    }
+
+    //根据不同的分组情况返回分组
+    public function team_group($teams_count, $teams)
+    {
+        $teams_group = [];
+        switch ($teams_count) {
+            case 12:
+                $teams_group['a'] = $teams->where('group', 'A');
+                $teams_group['b'] = $teams->where('group', 'B');
+                $teams_group['c'] = $teams->where('group', 'C');
+                return $teams_group;
+                break;
+
+            case 16:
+                $teams_group['a'] = $teams->where('group', 'A');
+                $teams_group['b'] = $teams->where('group', 'B');
+                $teams_group['c'] = $teams->where('group', 'C');
+                $teams_group['d'] = $teams->where('group', 'D');
+                return $teams_group;
+                break;
+
+            default:
+                return response('你输入情况有错误,请检查',404);
+                break;
+        }
+
+    }
+
+    //创建刚开始的小组赛对局
+    public function create_match($teams_a,$teams_b,$teams_human,$compare_id)
+    {
+        if ($teams_human == 3) {
+            //需要进行的总对局数量排列算法.
+            $teams_a_matches= $teams_human *($teams_human-1);
+             
+            //创建A组match
+            for ($i = 1; $i <= $teams_a_matches; $i++) {
+                $match = new Match();
+                $match->compare_id=$compare_id;
+                $match->round = 1;  //小组赛 直接写死.
+                $match->type ="小组赛";
+                
+            }
+            //创建B组match
+            for ($j = 1; $j <= $teams_a_matches; $j++){
+
+            }
         }
     }
 }
