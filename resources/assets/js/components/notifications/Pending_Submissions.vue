e92653c31e082d494cda29a4df87d76ceea2e58f
peng <531299585@qq.com>
2018-01-05 09:10:21 +0800

    修复投稿请求消息页面的样式问题

--- resources/assets/js/components/notifications/Pending_Submissions.vue
+++ resources/assets/js/components/notifications/Pending_Submissions.vue
@@ -8,44 +8,44 @@
 		</div>
 		<ul class="article_list">
 		    <li class="article_item have_img">
-		        <a class="wrap_img" href="/v1/detail" target="_blank">
+		        <a class="wrap_img" href="/v2/detail" target="_blank">
 		            <img src="/images/details_01.jpeg"/>
 		        </a>
 		        <div class="content">
 		            <div class="author">
-		                <a class="avatar" href="/v1/user" target="_blank">
+		                <a class="avatar" href="/v2/user" target="_blank">
 		                    <img src="/images/photo_02.jpg"/>
 		                </a>
-		                <div class="info">
-		                    <a href="/v1/user" target="_blank">
+		                <div class="info_meta">
+		                    <a href="/v2/user" target="_blank" class="nickname">
 		                        空评
 		                    </a>
-		                    <a href="/v1/detail" target="_blank">
-		                        <img src="/images/vip1.png"/>
+		                    <a href="/v2/detail" target="_blank">
+		                        <img src="/images/vip1.png" data-toggle="tooltip" data-placement="top" title="爱你城签约作者" class="badge_icon_xs" />
 		                    </a>
 		                    <span class="time">
 		                        2天前
 		                    </span>
 		                </div>
 		            </div>
-		            <a class="title" href="/v1/detail" target="_blank">
-		                为什么说被马化腾点赞的《王者荣耀》已成为全球最赚钱的游戏？
+		            <a class="headline paper_title" href="/v2/detail" target="_blank">
+		                <span>为什么说被马化腾点赞的《王者荣耀》已成为全球最赚钱的游戏？</span>
 		            </a>
 		            <p class="abstract">
 		                5月17日下午，腾讯控股公布了2017年第一季度财报。财报显示，腾讯一季度营收495.52亿元，同比增长55%；网络游戏收入增长34%至228.11亿元。其中，就智能手机游戏而言，腾讯实现129亿元收入，同比增长57%，此乃受现有及新的游戏如（《王者荣耀》、《穿越火线：枪战王者》及《龙之谷》）所推动。
 		            </p>
 		            <div class="meta">
-		                <a href="/v1/detail" target="_blank">
+		                <a href="/v2/detail" target="_blank" class="count count_link">
 		                    <i class="iconfont icon-liulan">
 		                    </i>
 		                    717
 		                </a>
-		                <a href="/v1/detail" target="_blank">
+		                <a href="/v2/detail" target="_blank" class="count count_link">
 		                    <i class="iconfont icon-svg37">
 		                    </i>
 		                    6
 		                </a>
-		                <span>
+		                <span class="count">
 		                    <i class="iconfont icon-03xihuan">
 		                    </i>
 		                    13
@@ -53,55 +53,55 @@
 		            </div>
 		        </div>
 		        <div class="push_action">
-		        	<a href="javascript:;" class="botm push">接受</a>
-		        	<a href="javascript:;" class="botm revoke">拒绝</a>
+		        	<a href="javascript:;" class="btn_base btn_push">接受</a>
+		        	<a href="javascript:;" class="btn_base btn_revoke">拒绝</a>
 		        	<span class="push_time">2017.12.11 16:27 投稿</span>
 		        </div>
 		    </li>
 		    <li class="article_item have_img">
-		        <a class="wrap_img" href="/v1/detail" target="_blank">
+		        <a class="wrap_img" href="/v2/detail" target="_blank">
 		            <img src="/images/details_02.jpeg"/>
 		        </a>
 		        <div class="content">
 		            <div class="author">
-		                <a class="avatar" href="/v1/user" target="_blank">
+		                <a class="avatar" href="/v2/user" target="_blank">
 		                    <img src="/images/photo_03.jpg"/>
 		                </a>
-		                <div class="info">
-		                    <a href="/v1/user" target="_blank">
+		                <div class="info_meta">
+		                    <a href="/v2/user" target="_blank" class="nickname">
 		                        闫浩
 		                    </a>
-		                    <a href="/v1/detail" target="_blank">
-		                        <img src="/images/vip1.png"/>
+		                    <a href="/v2/detail" target="_blank">
+		                        <img src="/images/vip1.png" data-toggle="tooltip" data-placement="top" title="爱你城签约作者" class="badge_icon_xs" />
 		                    </a>
 		                    <span class="time">
 		                        5天前
 		                    </span>
 		                </div>
 		            </div>
-		            <a class="title" href="/v1/detail" target="_blank">
-		                起风了，谁是下一个王者荣耀？
+		            <a class="headline paper_title" href="/v2/detail" target="_blank">
+		                <span>起风了，谁是下一个王者荣耀？</span>
 		            </a>
 		            <p class="abstract">
 		                下一个王者荣耀露出苗头很久了，今年夏天开始爆发。过去一年创投界有两个关注的焦点，一个是共享单车、共享充电宝带起来的线下流量入口，另一个是狼人杀和王者荣耀们的游戏社交化浪潮。这两个方向的创业者和巨头们，因缘际会地捕捉到了线上买量不再拥有性价比的历史脉搏，一方选择了开源，将物理投放的线下智能终端作为切入交易的新入口，另一方选择了节流，通过游戏本身的社交属性达成裂变式传播的效果，以较低的成本延长产品的生命周期以及活跃度。
 		            </p>
 		            <div class="meta">
-		                <a href="/v1/detail" target="_blank">
+		                <a href="/v2/detail" target="_blank" class="count count_link">
 		                    <i class="iconfont icon-liulan">
 		                    </i>
 		                    2325
 		                </a>
-		                <a href="/v1/detail" target="_blank">
+		                <a href="/v2/detail" target="_blank" class="count count_link">
 		                    <i class="iconfont icon-svg37">
 		                    </i>
 		                    5
 		                </a>
-		                <span>
+		                <span class="count">
 		                    <i class="iconfont icon-03xihuan">
 		                    </i>
 		                    61
 		                </span>
-		                <span>
+		                <span class="count">
 		                    <i class="iconfont icon-qianqianqian">
 		                    </i>
 		                    2
@@ -109,52 +109,49 @@
 		            </div>
 		        </div>
 		        <div class="push_action">
-		        	<a href="javascript:;" class="botm push">接受</a>
-		        	<a href="javascript:;" class="botm revoke">拒绝</a>
+		        	<a href="javascript:;" class="btn_base btn_push">接受</a>
+		        	<a href="javascript:;" class="btn_base btn_revoke">拒绝</a>
 		        	<span class="push_time">2017.12.11 16:27 投稿</span>
 		        </div>
 		    </li>
 		    <li class="article_item">
 		        <div class="content">
 		            <div class="author">
-		                <a class="avatar" href="/v1/user" target="_blank">
+		                <a class="avatar" href="/v2/user" target="_blank">
 		                    <img src="/images/photo_03.jpg"/>
 		                </a>
-		                <div class="info">
-		                    <a href="/v1/user" target="_blank">
+		                <div class="info_meta">
+		                    <a href="/v2/user" target="_blank" class="nickname">
 		                        浮云笑此生
-		                    </a>
-		                    <a href="/v1/detail" target="_blank">
-		                        <img src="/images/vip2.png"/>
 		                    </a>
 		                    <span class="time">
 		                        3周前
 		                    </span>
 		                </div>
 		            </div>
-		            <a class="title" href="/v1/detail" target="_blank">
-		                相亲与自由恋爱的比较
+		            <a class="headline paper_title" href="/v2/detail" target="_blank">
+		                <span>相亲与自由恋爱的比较</span>
 		            </a>
 		            <p class="abstract">
 		                这里的“相亲”主要指的是一种东亚现象。相亲有传统的因素，但它没有遭遇传统普遍的危机，反而是历久弥新。相亲是通过第三方来寻找配偶，第三方可以是父母辈的亲人，可以是同辈的亲友，也可以是中介机构。不管是经由谁介绍，所有相亲的共同特点是：它是一种挑选。由此，相亲也是一种面试，只不过是平等的、双向的面试。这种挑选分为三步进行，第一步是“初试”，这往往是由第三方完成，相亲者并不参与但可能会给第三方提供一些条件；第二步是“复试”，进入复试的往往有多人，此时相亲者互相见面并初步沟通，这也是相亲之所谓相亲的主要方面；第三步是“终试”，往往只有一人进入这个阶段，此时他们往往已经成为实际上的情侣关系，这也是最后考验，直到步入婚姻殿堂为止。
 		            </p>
 		            <div class="meta">
-		                <a href="/v1/detail" target="_blank">
+		                <a href="/v2/detail" target="_blank" class="count count_link">
 		                    <i class="iconfont icon-liulan">
 		                    </i>
 		                    1795
 		                </a>
-		                <a href="/v1/detail" target="_blank">
+		                <a href="/v2/detail" target="_blank" class="count count_link">
 		                    <i class="iconfont icon-svg37">
 		                    </i>
 		                    5
 		                </a>
-		                <span>
+		                <span class="count">
 		                    <i class="iconfont icon-03xihuan">
 		                    </i>
 		                    35
 		                </span>
-		                <span>
+		                <span class="count">
 		                    <i class="iconfont icon-qianqianqian">
 		                    </i>
 		                    7
@@ -162,8 +159,8 @@
 		            </div>
 		        </div>
 		        <div class="push_action">
-		        	<a href="javascript:;" class="botm push">接受</a>
-		        	<a href="javascript:;" class="botm revoke">拒绝</a>
+		        	<a href="javascript:;" class="btn_base btn_push">接受</a>
+		        	<a href="javascript:;" class="btn_base btn_revoke">拒绝</a>
 		        	<span class="push_time">2017.12.11 16:27 投稿</span>
 		        </div>
 		    </li>
@@ -184,5 +181,47 @@ export default {
 }
 </script>
 
-<style lang="css" scoped>
+<style lang="scss" scoped>
+	#pending_submissions {
+        .push_top {
+            position: fixed;
+            z-index: 1;
+            width: 780.01px;
+            min-height: 35px;
+            text-align: center;
+            background-color: #fff;
+            border-bottom: 1px solid #f0f0f0;
+            margin-bottom: 20px;
+            padding-bottom: 10px;
+            &::before {
+                content: "";
+                position: absolute;
+                top: -81px;
+                left: 0;
+                width: 100%;
+                height: 81px;
+                background-color: #fff;
+            }
+            .back_list {
+                font-size: 14px;
+                color: #969696;
+                float: left;
+                i {
+                    font-size: inherit;
+                }
+                &:hover {
+                    color: #333;
+                }
+            }
+            @media screen and (max-width: 1200px) {
+                width: 580.01px;
+            }
+            @media screen and (max-width: 992px) {
+                width: 489.99px;
+            }
+        }
+        .article_list {
+            padding-top: 50px;
+        }
+    }
 </style>
