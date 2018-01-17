2bdbbe1ad3cb22dec40b0ef220529c04d50f159d
peng <531299585@qq.com>
2018-01-17 11:05:44 +0800

    更改用户个人资料页面向后台传递数据的方式,避免引起数据问题

--- resources/assets/js/components/setting/Profile.vue
+++ resources/assets/js/components/setting/Profile.vue
@@ -21,15 +21,15 @@
 					<td class="top_line setting_title">性别(此处只可以修改哦)</td>
 					<td class="top_line">
 						<div>
-							<input type="radio" name="gender" value="男" v-model="inputtext.gender"/>
+							<input type="radio" name="gender" value="男" v-model="user.gender"/>
 							<span>男</span>
 						</div>
 						<div>
-							<input type="radio" name="gender" value="女" v-model="inputtext.gender"/>
+							<input type="radio" name="gender" value="女" v-model="user.gender"/>
 							<span>女</span>
 						</div>
 						<div>
-							<input type="radio" name="gender" value="保密" v-model="inputtext.gender"/>
+							<input type="radio" name="gender" value="保密" v-model="user.gender"/>
 							<span>保密</span>
 						</div>
 					</td>
@@ -38,7 +38,7 @@
 				<tr>
 					<td class="setting_title pull-left">个人简介</td>
 					<td>
-							<textarea :placeholder="user.introduction" cols="30" rows="10" class="form-control" v-model="inputtext.introduction"></textarea>
+							<textarea :placeholder="user.introduction" cols="30" rows="10" class="form-control" v-model="user.introduction"></textarea>
 					</td>
 				</tr>
 
@@ -81,11 +81,11 @@ export default {
   	   updateing(){
   	   	 var api=window.tokenize('/api/user/'+window.current_user_id+'/update');
   	   	 var vm=this;
-         var formdata=new FormData();
 
-
-         formdata.append('gender',this.inputtext.gender);
-         formdata.append('introduction',this.inputtext.introduction);
+         var formdata={
+              gender:this.user.gender,
+              introduction:this.user.introduction,
+         };
 
          window.axios.post(api,formdata).then(function(response){
          	   response.data==0 ? vm.fail=true : vm.updated=true;
@@ -98,7 +98,6 @@ export default {
         fail:false,
         updated:false,
         user:[],
-        inputtext:{}
     }
   }
 }
