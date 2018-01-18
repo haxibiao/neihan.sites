0bbcc53665d29eb233e4929b204a77b8f50db8c5
peng <531299585@qq.com>
2018-01-18 16:04:05 +0800

    更换爱你城首页logo

--- resources/views/parts/head_user.blade.php
+++ resources/views/parts/head_user.blade.php
@@ -16,7 +16,7 @@
                         </span>
                     </button>
                     <a class="navbar-brand" href="/">
-                        <img alt="Logo" src="/logo/ainicheng.com.jpg"/>
+                        <img alt="Logo" src="/logo/ainicheng.com.index.jpg"/>
                     </a>
                 </div>
                 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
