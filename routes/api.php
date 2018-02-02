66d77f6430a81ed3ad78f737814cb7ab95bd78b1
peng <531299585@qq.com>
2018-02-02 15:30:31 +0800

    整理api路由中的代码

--- routes/api.php
+++ routes/api.php
@@ -37,10 +37,7 @@ Route::get('/comment/{id}/{type}', 'Api\CommentController@get');
 
 
 //follow
-Route::middleware('auth:api')->post('/follow/{id}/{type}','Api\FollowController@toggle');
-Route::middleware('auth:api')->get('/follows', 'Api\FollowController@follows');
-Route::middleware('auth:api')->get('/follow/recommends', 'Api\FollowController@recommends');
-Route::middleware('auth:api')->get('/follow/{id}/{type}', 'Api\FollowController@touch');
+require_once 'api/follow.php';
 
 //notifications
 Route::middleware('auth:api')->get('notification/chats', 'Api\NotificationController@chats');
