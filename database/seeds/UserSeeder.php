08e05ba76d0bb5f8a00b0bee9b72a972004035c7
peng <531299585@qq.com>
2018-03-16 10:21:47 +0800

    删除莫名seed出来的数据 seed中的插入用户的代码删除

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -12,14 +12,11 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        // $users = User::all();
-        // foreach ($users as $user) {
-        //     $user->api_token = str_random(60);
-        //     $user->avatar    = $user->avatar();
-        //     $user->save();
-        // }
-
-        $users = factory(User::class)->times(100)->make();
-        User::insert($users->makeVisible(['password', 'remember_token'])->toArray());
+        $users = User::all();
+        foreach ($users as $user) {
+            $user->api_token = str_random(60);
+            $user->avatar    = $user->avatar();
+            $user->save();
+        }
     }
 }
