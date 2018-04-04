5e866ad6246dbc3ca1c020d5d2822e97b94eeac4
peng <531299585@qq.com>
2018-04-04 16:48:52 +0800

    修正首页算法 防止重复出现置顶文章

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -56,6 +56,7 @@ class IndexController extends Controller
             ->whereHas('User', function ($q) {
                 $q->where('is_editor', 1);
             })
+            ->whereNotIn('id',$articles_top)
             ->orderBy('updated_at', 'desc')
             ->where('status', '>', 0)
             ->paginate(10);
