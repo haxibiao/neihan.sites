8123d26ce1f2980052d6cb5d6059c5a9aa839dc3
ivan <ivan@haxibiao.com>
2018-05-17 11:40:20 +0800

    fix article edit miss images ...

--- pull.sh
+++ pull.sh
@@ -2,31 +2,37 @@
 
 
 cd ../dongmeiwei.com
+git checkout .
 git pull
 composer install
 php artisan env:refresh --local
 
 cd ../dongdianyao.com
+git checkout .
 git pull
 composer install
 php artisan env:refresh --local
 
 cd ../dongmeiwei.com
+git checkout .
 git pull
 composer install
 php artisan env:refresh --local
 
 cd ../qunyige.com
+git checkout .
 git pull
 composer install
 php artisan env:refresh --local
 
 cd ../youjianqi.com
+git checkout .
 git pull
 composer install
 php artisan env:refresh --local
 
 cd ../dianmoge.com
+git checkout .
 git pull
 composer install
 php artisan env:refresh --local
--- status.sh
+++ status.sh
@@ -1,19 +1,25 @@
 #!bin/bash
 
+echo "dongmeiwei status ..."
 cd ../dongmeiwei.com
 git status
 
+echo "dongdianyao status ..."
 cd ../dongdianyao.com
 git status
 
-cd ../dongmeiwei.com
+echo "dongdianyi status ..."
+cd ../dongdianyi.com
 git status
 
+echo "qunyige status ..."
 cd ../qunyige.com
 git status
 
+echo "youjianqi status ..."
 cd ../youjianqi.com
 git status
 
+echo "dianmoge status ..."
 cd ../dianmoge.com
 git status
