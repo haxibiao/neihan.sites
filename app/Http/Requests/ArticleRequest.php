012916d2095c883d21e49c614d75e73775ebe44d
peng <531299585@qq.com>
2018-05-22 15:51:17 +0800

    按照pm的要求,编辑用编辑器发布不验证是否粘贴图片

--- app/Http/Requests/ArticleRequest.php
+++ app/Http/Requests/ArticleRequest.php
@@ -33,7 +33,7 @@ class ArticleRequest extends FormRequest
             'title'       => 'required',
             // 'keywords'    => 'required',
             // 'description' => 'required|min:10',
-            'body'        => 'required|min:20|not_copyed_image',
+            'body'        => 'required|min:20',
         ];
     }
 }
