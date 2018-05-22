ba22e59f7b070c68b410d91307d48c9abe836d1f
邹敏 <zmin@zoumindeMac-mini.local>
2018-05-22 16:31:23 +0800

    修复头部搜索框的双击提示框bug

--- resources/assets/js/components/search/SearchBox.vue
+++ resources/assets/js/components/search/SearchBox.vue
@@ -1,22 +1,21 @@
 <template>
-	<form class="navbar-form navbar-left" role="search" action="/search" method="get" id='searchForm'>
-		<div class="form-group">
-			<div class="search-wrapper">
-				<input type="text" id="search-input" class="form-control" placeholder="搜索" name="q">
-				<span class="iconfont icon-sousuo search-btn" @click="search"></span>
-				<div class="hot-search-wrp hidden-xs">
-					<hot-search></hot-search>
-					<recently v-if="user" hide-title="true"></recently>
-				</div>
-			</div>
-		</div>
-	</form>
+  <form class="navbar-form navbar-left" role="search" action="/search" method="get" id='searchForm'>
+    <div class="form-group">
+      <div class="search-wrapper">
+        <input type="text" id="search-input" class="form-control" placeholder="搜索" name="q" autocomplete="off">
+        <span class="iconfont icon-sousuo search-btn" @click="search"></span>
+        <div class="hot-search-wrp hidden-xs">
+          <hot-search></hot-search>
+          <recently v-if="user" hide-title="true"></recently>
+        </div>
+      </div>
+    </div>
+  </form>
 </template>
 
 <script>
 export default {
-
-  name: 'SearchBox',
+  name: "SearchBox",
 
   computed: {
     user() {
@@ -25,47 +24,47 @@ export default {
   },
 
   methods: {
-  	search() {
-  		$('#searchForm').submit();
-  	}
+    search() {
+      $("#searchForm").submit();
+    }
   },
 
-  data () {
-    return {
-    }
+  data() {
+    return {};
   }
-}
+};
 </script>
 
 <style lang="scss">
 .hot-search-wrp {
-    margin-top: 9px;
-    width: 220px;
-    left: 0;
-    top: 100%;
-    visibility: hidden;
-    opacity: 0;
-    border-radius: 4px;
-    z-index: 999;
-    &,&::before {
-    	position: absolute;
-	    background-color: #fff;
-	    box-shadow: 0 0 8px rgba(0,0,0,.2);
-    }
-    &::before {
-    	content: "";
-    	left: 27px;
-    	width: 10px;
-    	height: 10px;
-    	transform: rotate(45deg);
-    	top: -5px;
-    	z-index: -1;
-    }
-    .hot-search {
-    	border-bottom: 1px solid #f0f0f0;
-    }
-    .search-recent {
-    	padding: 10px 15px;
-    }
+  margin-top: 9px;
+  width: 220px;
+  left: 0;
+  top: 100%;
+  visibility: hidden;
+  opacity: 0;
+  border-radius: 4px;
+  z-index: 999;
+  &,
+  &::before {
+    position: absolute;
+    background-color: #fff;
+    box-shadow: 0 0 8px rgba(0, 0, 0, 0.2);
+  }
+  &::before {
+    content: "";
+    left: 27px;
+    width: 10px;
+    height: 10px;
+    transform: rotate(45deg);
+    top: -5px;
+    z-index: -1;
+  }
+  .hot-search {
+    border-bottom: 1px solid #f0f0f0;
+  }
+  .search-recent {
+    padding: 10px 15px;
+  }
 }
 </style>
