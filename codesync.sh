20fff4054189f9f849d218b806ddd7298f188999
ivan <ivan@haxibiao.com>
2018-05-30 12:02:48 +0800

    sync

--- codesync.sh
+++ codesync.sh
@@ -9,6 +9,9 @@ echo "sass code except theme ..."
 sudo /bin/cp -rf /data/www/ainicheng.com/resources/assets/sass /data/www/$1.com/resources/assets/
 git checkout resources/assets/sass/_theme.scss
 
+echo 'webpack.mix.js ...'
+sudo /bin/cp -rf /data/www/ainicheng.com/webpack.mix.js /data/www/$1.com/
+
 sudo /bin/cp -rf /data/www/ainicheng.com/app /data/www/$1.com/
 sudo /bin/cp -rf /data/www/ainicheng.com/database/migrations /data/www/$1.com/database/
 sudo /bin/cp -rf /data/www/ainicheng.com/resources/views /data/www/$1.com/resources/
--- pull.sh
+++ pull.sh
@@ -2,37 +2,37 @@
 
 
 cd ../dongmeiwei.com
-git checkout .
+git stash -u
 git pull
 composer install
 php artisan env:refresh --local
 
 cd ../dongdianyao.com
-git checkout .
+git stash -u
 git pull
 composer install
 php artisan env:refresh --local
 
 cd ../dongmeiwei.com
-git checkout .
+git stash -u
 git pull
 composer install
 php artisan env:refresh --local
 
 cd ../qunyige.com
-git checkout .
+git stash -u
 git pull
 composer install
 php artisan env:refresh --local
 
 cd ../youjianqi.com
-git checkout .
+git stash -u
 git pull
 composer install
 php artisan env:refresh --local
 
 cd ../dianmoge.com
-git checkout .
+git stash -u
 git pull
 composer install
 php artisan env:refresh --local
