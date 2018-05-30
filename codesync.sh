4a4f19970a68208972def82086eb219a2cf07952
ivan <ivan@haxibiao.com>
2018-05-30 18:01:05 +0800

    fix seed

--- codesync.sh
+++ codesync.sh
@@ -13,6 +13,7 @@ echo 'webpack.mix.js ...'
 sudo /bin/cp -rf /data/www/ainicheng.com/webpack.mix.js /data/www/$1.com/
 
 sudo /bin/cp -rf /data/www/ainicheng.com/app /data/www/$1.com/
+rm -rf /data/www/$1.com/database/migrations/*
 sudo /bin/cp -rf /data/www/ainicheng.com/database/migrations /data/www/$1.com/database/
 sudo /bin/cp -rf /data/www/ainicheng.com/resources/views /data/www/$1.com/resources/
 sudo /bin/cp -rf /data/www/ainicheng.com/resources/assets/js /data/www/$1.com/resources/assets/
