6b19c27d256561a9fed3e5bef7a8cc54d0f0b2aa
peng <531299585@qq.com>
2018-05-31 10:54:53 +0800

    修复支付宝打赏的时候回调缺少字段的问题

---
+++ database/migrations/2018_05_31_104912_add_column_message_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnMessageTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tips', function (Blueprint $table) {
+            $table->text('message')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tips', function (Blueprint $table) {
+            //
+        });
+    }
+}
