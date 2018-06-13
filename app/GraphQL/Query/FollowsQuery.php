6c942e57703b7763c770b32a5a6923dd42d35d7d
ivan <ivan@haxibiao.com>
2018-06-13 18:26:26 +0800

    graphql: 实现用户关注的用户，专题，文集query

--- app/GraphQL/Query/FollowsQuery.php
+++ app/GraphQL/Query/FollowsQuery.php
@@ -21,27 +21,34 @@ class FollowsQuery extends Query
     public function args()
     {
         return [
-            'user_id' => ['name' => 'user_id', 'type' => Type::int()],
-            'limit'   => ['name' => 'limit', 'type' => Type::int()],
-            'offset'  => ['name' => 'offset', 'type' => Type::int()],
-            'filter'  => ['name' => 'filter', 'type' => GraphQL::type('FollowFilter')],
+            'user_id'               => ['name' => 'user_id', 'type' => Type::int()],
+            'recommend_for_user_id' => ['name' => 'recommend_for_user_id', 'type' => Type::int()],
+            'limit'                 => ['name' => 'limit', 'type' => Type::int()],
+            'offset'                => ['name' => 'offset', 'type' => Type::int()],
+            'filter'                => ['name' => 'filter', 'type' => GraphQL::type('FollowFilter')],
         ];
     }
 
     public function resolve($root, $args)
     {
 
-        $qb = Follow::orderBy('id', 'desc'); //consider distinct ...
+        $qb = Follow::orderBy('id', 'desc'); //TODO:: consider distinct ...  这个留着后面推荐系统来解决
 
         if (isset($args['user_id'])) {
-            $qb = $qb->where('user_id', '<>', $args['user_id']);
+            $qb = $qb->where('user_id', $args['user_id']); //不是你关注的，别人关注了的，就是可以推荐给你的
+        }
+
+        if (isset($args['recommend_for_user_id'])) {
+            $qb = $qb->where('user_id', '<>', $args['recommend_for_user_id']); //不是你关注的，别人关注了的，就是可以推荐给你的
         }
 
         if (isset($args['filter'])) {
             if ($args['filter'] == 'USER') {
                 $qb = $qb->where('followed_type', 'users');
-            } elseif ($args['filter'] == 'CATEGORY') {
+            } else if ($args['filter'] == 'CATEGORY') {
                 $qb = $qb->where('followed_type', 'categories');
+            } else if ($args['filter'] == 'COLLECTION') {
+                $qb = $qb->where('followed_type', 'collections');
             }
         }
 
