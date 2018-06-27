f6159bf8d058f6640dda64ae213c94851bdc62f3
XXM <xiaoxinming@haxibiao.com>
2018-06-27 17:20:13 +0800

    修复爱你城搜索->专题页  无法正常关注专题

--- resources/views/search/categories.blade.php
+++ resources/views/search/categories.blade.php
@@ -14,7 +14,12 @@
 						@foreach($data['categories'] as $category) 
 						<li class="note-info"><a href="/{{ $category->name_en }}" class="avatar-category">
 							<img src="{{ $category->logo() }}" alt=""></a>
-							<follow></follow>
+							<follow 
+					            type="categories" 
+					            id="{{$category->id}}" 
+					            user-id="{{ user_id() }}" 
+					            followed="{{ is_follow('categories', $category->id) }}">  
+					          </follow>
 							<div class="title"><a href="/{{ $category->name_en }}" class="name">{{ $category->name }}</a></div>
 							<div class="info"><p>收录了{{ $category->count }}篇文章，{{ $category->count_follows }}人关注</p></div></li>
 						@endforeach
