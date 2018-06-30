9d737eed486c2a525435ac77823d77bcc8f7bdb1
czg <caizhigang@haxibiao.com>
2018-06-30 13:37:14 +0800

    删除videos表中的字段(不能与上一个提交合并)

---
+++ database/migrations/2018_06_30_132836_alert_video_step2.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlertVideoStep2 extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('videos', function (Blueprint $table) {
+            $table->dropColumn([
+                'hits',
+                'category_id',  
+                'introduction',
+                'path_mp4',  
+                'count',
+                'likes',
+                'cover'
+            ]);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('videos', function (Blueprint $table) {
+            //
+        });
+    }
+}
