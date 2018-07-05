90e15846f4d0c9d96cafc3b311f96ecb48021b08
czg <caizhigang@haxibiao.com>
2018-07-05 14:11:24 +0800

    PC:修复专题页显示问题

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -21,34 +21,38 @@
           <h3 class="panel-title" style="min-height: 50px">专题管理</h3>
         </div>
         <div class="panel-body small">
-          @foreach($categories as $category)
-            <div class="top10 col-xs-6 col-md-4 col-lg-3">
-              <div class="pull-right">
+          @foreach($categories->chunk(3) as $category_group)
+            <div class="row">
+              @foreach ($category_group as $category)
+                  <div class="top10 col-xs-6 col-md-4 col-lg-3">
+                    <div class="pull-right">
 
-                {!! Form::open(['method' => 'get', 'route' => ['category.edit', $category->id], 'class' => 'form-horizontal']) !!}
-                    <div class="btn-group pull-right right10">
-                        {!! Form::submit("编辑", ['class' => 'btn btn-sm btn-primary']) !!}
-                    </div>        
-                {!! Form::close() !!}
+                      {!! Form::open(['method' => 'get', 'route' => ['category.edit', $category->id], 'class' => 'form-horizontal']) !!}
+                          <div class="btn-group pull-right right10">
+                              {!! Form::submit("编辑", ['class' => 'btn btn-sm btn-primary']) !!}
+                          </div>        
+                      {!! Form::close() !!}
 
-              </div>   
-                <h5>{{ $category->name }} ({{ $category->name_en }})</h5>
-                <img src="{{ $category->logo() }}" alt="">
-                <div class="pull-right">
-                  
-                  {!! Form::open(['method' => 'delete', 'route' => ['category.destroy', $category->id], 'class' => 'form-horizontal']) !!}
-                      <div class="btn-group pull-right">
-                          {!! Form::submit("删除", ['class' => 'btn btn-sm btn-danger']) !!}
-                      </div>    
-                  {!! Form::close() !!}
-                </div>
-                <p>创建人: {{ $category->user->name }} </p>
-                <p style="min-height: 100px" class="small well">{{ $category->description }}</p>
+                    </div>   
+                      <h5>{{ $category->name }} ({{ $category->name_en }})</h5>
+                      <img src="{{ $category->logo() }}" alt="">
+                      <div class="pull-right">
+                        
+                        {!! Form::open(['method' => 'delete', 'route' => ['category.destroy', $category->id], 'class' => 'form-horizontal']) !!}
+                            <div class="btn-group pull-right">
+                                {!! Form::submit("删除", ['class' => 'btn btn-sm btn-danger']) !!}
+                            </div>    
+                        {!! Form::close() !!} 
+                      </div>
+                      <p>创建人: {{ $category->user->name }} </p>
+                      <p style="min-height: 100px" class="small well">{{ $category->description }}</p>
+                  </div>
+              @endforeach
             </div>
           @endforeach
         </div>
         <div class="panel-footer">
-          {!! $categories->links() !!
+          {!! $categories->links() !!}
         </div>
       </div>  
   
