046f567445701d83b77c10c77dbe6339ac27fec3
leikun <leikun@haxibiao.com>
2018-07-09 14:30:32 +0800

    给专题和文章管理加一个搜索，有助于运营人员管理，节约时间。优化了管理专题下的ui布局看着更舒服一些。。。

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -14,17 +14,19 @@
 
 <div class="container">
       <div class="panel panel-default">
-        <div class="panel-heading">
+        <div class="panel-heading category-admin-header">
           <div class="pull-right">
             <a class="btn btn-primary" href="/category/create{{ Request::get('type') ? '?type='.Request::get('type') : '' }}" role="button">添加分类</a>
           </div>
-          <h3 class="panel-title" style="min-height: 50px">专题管理</h3>
+          <h3 class="panel-title category-admin-title">专题管理</h3>
+          <basic-search></basic-search>
         </div>
         <div class="panel-body small">
           @foreach($categories->chunk(3) as $category_group)
             <div class="row">
               @foreach ($category_group as $category)
-                  <div class="top10 col-xs-6 col-md-4 col-lg-3">
+                  <div class=" category-item top10 col-xs-12 clearfix">
+                     <img src="{{ $category->logo() }}" alt="" class="pull-left">
                     <div class="pull-right">
 
                       {!! Form::open(['method' => 'get', 'route' => ['category.edit', $category->id], 'class' => 'form-horizontal']) !!}
@@ -32,20 +34,22 @@
                               {!! Form::submit("编辑", ['class' => 'btn btn-sm btn-primary']) !!}
                           </div>        
                       {!! Form::close() !!}
-
-                    </div>   
-                      <h5>{{ $category->name }} ({{ $category->name_en }})</h5>
-                      <img src="{{ $category->logo() }}" alt="">
-                      <div class="pull-right">
+                    </div>
+                    <div class="pull-right">
                         
                         {!! Form::open(['method' => 'delete', 'route' => ['category.destroy', $category->id], 'class' => 'form-horizontal']) !!}
                             <div class="btn-group pull-right">
                                 {!! Form::submit("删除", ['class' => 'btn btn-sm btn-danger']) !!}
                             </div>    
                         {!! Form::close() !!} 
+                      </div>   
+                      
+                     
+                      <div class="item-info">
+                        <h5 class="category-name">{{ $category->name }} ({{ $category->name_en }})</h5>
+                        <span>创建人: {{ $category->user->name }} </span>
+                        <p style="min-height: 100px" class="small well">{{ $category->description }}</p>
                       </div>
-                      <p>创建人: {{ $category->user->name }} </p>
-                      <p style="min-height: 100px" class="small well">{{ $category->description }}</p>
                   </div>
               @endforeach
             </div>
