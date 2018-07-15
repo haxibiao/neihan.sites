5b303cf5e3c8fb8818fa6db70ae9d7a203efa7e1
leikun <leikun@haxibiao.com>
2018-07-15 17:55:54 +0800

    修复头部视频栏时获取active样式。。。

--- resources/views/parts/header_guest.blade.php
+++ resources/views/parts/header_guest.blade.php
@@ -33,7 +33,7 @@
 					<div class="collapse navbar-collapse navbar-ex1-collapse">
 						<ul class="nav navbar-nav">
 							<li class="tab {{ get_active_css('/') }}"><a href="/"><i class="iconfont icon-faxian hidden-xs hidden-md"></i><span class="hidden-sm">首页</span></a></li>
-							<li class="tab {{ get_active_css('question') }}"><a target="{{ \Agent::isDeskTop()? '_blank':'_self' }}" href="/video"><i class="iconfont icon-shipin2 hidden-xs hidden-md"></i><span class="hidden-sm">视频</span></a></li>
+							<li class="tab {{ get_active_css('video') }}"><a target="{{ \Agent::isDeskTop()? '_blank':'_self' }}" href="/video"><i class="iconfont icon-shipin2 hidden-xs hidden-md"></i><span class="hidden-sm">视频</span></a></li>
 							<li class="tab {{ get_active_css('app') }}"><a target="{{ \Agent::isDeskTop()? '_blank':'_self' }}" href="/app"><i class="iconfont icon-ordinarymobile hidden-xs hidden-md"></i><span class="hidden-sm">下载App</span></a></li>
 						</ul>
 						<search-box is-desktop="{{ \Agent::isDeskTop() == 1 }}"></search-box>
