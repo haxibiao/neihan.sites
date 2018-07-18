3c390860c58be25d2197439f612fffc932b7565f
XXM <xiaoxinming@haxibiao.com>
2018-07-18 16:16:15 +0800

    视频绑定视频标题

--- resources/views/video/parts/hot_category_video.blade.php
+++ resources/views/video/parts/hot_category_video.blade.php
@@ -24,6 +24,7 @@
                         <img class="video-photo" src=" {{ $article->image_url }}">
                         <i class="hover-play"> </i>
                     </a>
+                    <a href="/video/{{ $article->video_id }}">{{$article->title}}</a>
                     <div class="info">
                         <a class="user" href="/user/{{ $article->user_id }}">
                             <img src="{{ $article->user->avatar() }}" class="avatar">
@@ -36,40 +37,6 @@
                     </div>          
                 </li>
             @endforeach
-            {{-- <li class="game-video-item">
-                <a href="/video/333" class="video-info">   
-                    <img class="video-photo" src="https://www.ainicheng.com/storage/video/333.jpg">
-                     <i class="hover-play"> </i>
-                </a>
-                <a href="">在绝地求生和F4一起来看“流星雨”</a>
-                <div class="info">
-                    <a class="user" href="/user/270">
-                        <img src="https://ainicheng.com/storage/avatar/270.jpg" class="avatar">
-                        <span>风清歌</span>                          
-                    </a>
-                    <div class="num">
-                        <i class="iconfont icon-liulan"> 11</i>
-                        <i class="iconfont icon-svg37"> 20</i>
-                    </div>
-                </div>
-            </li>
-            <li class="game-video-item">
-                <a href="/video/291" class="video-info">   
-                    <img  class="video-photo" src="https://www.ainicheng.com/storage/video/291.jpg">
-                    <i class="hover-play"> </i>
-                </a>
-                 <a href="">绝地求生大逃杀新手教学</a>
-                <div class="info">
-                    <a class="user" href="/user/270">
-                        <img src="https://ainicheng.com/storage/avatar/270.jpg" class="avatar">
-                        <span>风清歌</span>                          
-                    </a>
-                    <div class="num">
-                        <i class="iconfont icon-liulan"> 11</i>
-                        <i class="iconfont icon-svg37"> 20</i>
-                    </div>
-                </div>
-            </li> --}}
         </ul>
     </div>
 </div>  
