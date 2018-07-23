32ca9d4138c90578bb07708f73e9fe7513921522
XXM <xiaoxinming@haxibiao.com>
2018-07-23 17:33:02 +0800

    up ....

--- resources/views/category/parts/related.blade.php
+++ resources/views/category/parts/related.blade.php
@@ -1,11 +1,13 @@
 <div class="atten-people">
 	<p class="plate-title">相关专题</p>
-    <ul class="list-people">         
-    	 <li class="single-media">
-    	 	<a href="/nishuihan" class="avatar-category">
-	    	 	<img src="https://ainicheng.com/storage/category/94.logo.jpg" alt="逆水寒">
-	    	 </a>
-    	 	<a href="/nishuihan" class="info">逆水寒</a>
-    	</li>
+    <ul>
+    	@foreach($data['related_category'] as $category)         
+	    	 <li class="single-media">
+	    	 	<a href="/{{ $category->name_en }}" class="avatar-category">
+		    	 	<img src="{{ $category->logo }}" alt="{{ $category->name }}">
+		    	 </a>
+	    	 	<a href="/{{ $category->name_en }}" class="info">{{ $category->name }}</a>
+	    	</li>
+    	@endforeach
     </ul>
 </div>
