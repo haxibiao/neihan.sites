97c625e93cff74e6c344717871dff74f829b4731
leikun <leikun@haxibiao.com>
2018-07-24 16:00:16 +0800

    个人主页内我管理的专题下可以查看所有的我管理的专题了

--- resources/views/user/aside/managed_categories.blade.php
+++ resources/views/user/aside/managed_categories.blade.php
@@ -2,7 +2,7 @@
 	<p class="plate-title">{{ $user->ta() }}管理的专题</p>
 
 	<ul class="admin-category">
-		@foreach($user->adminCategories()->orderBy('id','desc')->take(10)->get() as $category)
+		@foreach($user->adminCategories()->orderBy('id','desc')->get() as $category)
 
 		<li class="single-media"><a href="/{{ $category->name_en }}" class="avatar-category"><img src="{{ $category->logo() }}" alt="{{ $category->name }}"></a><a href="/{{ $category->name_en }}" class="info">{{ $category->name }}</a></li>	
 		@endforeach	
