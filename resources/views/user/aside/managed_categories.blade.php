442ea518f982085a71e10040bcf7ab9a05b973a4
XXM <xiaoxinming@haxibiao.com>
2018-07-24 13:36:18 +0800

    返回所有的专题数据

--- resources/views/user/aside/managed_categories.blade.php
+++ resources/views/user/aside/managed_categories.blade.php
@@ -1,7 +1,7 @@
 <div class="administrator distance">
 	<p class="plate-title">{{ $user->ta() }}管理的专题</p>
 	<ul>
-		@foreach($user->adminCategories()->orderBy('id','desc')->take(5)->get() as $category)
+		@foreach($user->adminCategories()->orderBy('id','desc')->get() as $category)
 		<li class="single-media"><a href="/{{ $category->name_en }}" class="avatar-category"><img src="{{ $category->logo() }}" alt="{{ $category->name }}"></a><a href="/{{ $category->name_en }}" class="info">{{ $category->name }}</a></li>	
 		@endforeach
 	</ul>
