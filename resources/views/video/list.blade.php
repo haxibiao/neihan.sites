e0a2050d041940b002b9bd40f7b0e414dc6fd2b0
XXM <xiaoxinming@haxibiao.com>
2018-07-31 11:24:44 +0800

    添加了上架 下架Button

--- resources/views/video/list.blade.php
+++ resources/views/video/list.blade.php
@@ -39,8 +39,14 @@
                         <div class="media-body">
                             @if(checkEditor())
                             <div class="pull-right">
-                              {!! Form::open(['method' => 'delete', 'route' => ['video.destroy', $video->id], 'class' => 'form-horizontal pull-left']) !!}
-                                {!! Form::submit('删除', ['class' => 'btn btn-default btn-small']) !!}                
+                              {!! Form::open(['method' => 'PUT', 'route' => ['video.update', $video->id], 'class' => 'form-horizontal pull-left']) !!}
+                                @if($article->status == 0)
+                                {!! Form::hidden('status', 1) !!}
+                                {!! Form::submit('上架', ['class' => 'btn btn-success btn-small']) !!}
+                                @else 
+                                {!! Form::hidden('status', 0) !!}
+                                {!! Form::submit('下架', ['class' => 'btn btn-default btn-small']) !!}
+                                @endif             
                               {!! Form::close() !!}
                                 <a class="btn btn-primary btn-small" href="/video/{{ $video->id }}/edit" role="button" style="margin-left: 5px">
                                     编辑
@@ -68,6 +74,10 @@
                                     <span class="label label-success">截图已完成</span>
                                 @else
                                     <span class="label label-default">截图ing...</span>
+                                @endif
+                                
+                                @if($article->status == 0) 
+                                <span class="label label-info">已下架</span>
                                 @endif
                             </p>
                         </div>
