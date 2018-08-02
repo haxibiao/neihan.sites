b4f2877477c631e63267d6f539c3d94e15ab44b4
XXM <xiaoxinming@haxibiao.com>
2018-08-02 15:21:47 +0800

    up...

--- resources/views/user/videos.blade.php
+++ resources/views/user/videos.blade.php
@@ -12,7 +12,7 @@
       </ol>
     <div class="panel panel-default">
         <div class="panel-body">
-            <img alt="" class="img img-circle avatar_small" src="{{ $user->avatar() }}">
+            <img alt="{{ $user->name }}" width="100px" class="img img-circle" src="{{ $user->avatar() }}">
                 <h4>
                     {{ $user->name }}
                 </h4>
