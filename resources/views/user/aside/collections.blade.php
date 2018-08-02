9e83db0791e2056f44387bf56d2c5cd2ba2c492d
高璇 <gaoxuan@gaoxuandeMac-mini.local>
2018-08-02 14:00:26 +0800

    修复个人主页文集人称显示错误

--- resources/views/user/aside/collections.blade.php
+++ resources/views/user/aside/collections.blade.php
@@ -1,5 +1,5 @@
 <div class="collection distance">
-    <p class="plate-title">他的文集</p>
+    <p class="plate-title">{{ $user->ta() }}的文集</p>
     <ul class="icon-text-list">
     	@foreach($user->collections()->orderBy('id','desc')->whereStatus(1)->take(5)->get() as $collection)
         <li><a href="/collection/{{ $collection->id }}"><i class="iconfont icon-wenji"></i> <span>{{ $collection->name }}</span></a></li>
