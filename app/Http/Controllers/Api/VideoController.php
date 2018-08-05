56ce96e9702742d4d9937a704d9a2174ed4dda04
ivan <ivan@haxibiao.com>
2018-08-05 12:23:40 +0800

    重构一下 /api/video/save, 发现继续兼容web sdk 上传视频后的接口，一样的

--- app/Http/Controllers/Api/VideoController.php
+++ app/Http/Controllers/Api/VideoController.php
@@ -81,54 +81,55 @@ class VideoController extends Controller
         //如果是通过表单上传文件
         $file = $request->video;
         if ($file) {
-            $hash  = md5_file($file->path());
-            $video = Video::firstOrNew([
-                'hash' => $hash,
-            ]);
-            if ($video->id) {
-                // abort(505,"相同视频已存在");
-            }
-            $video->title = $file->getClientOriginalName();
-            $video->save();
-            //虽然是简单的上传文件但是文章关系还是存了下来
-            $article            = new Article();
-            $article->user_id   = getUserId();
-            $article->video_url = $video->getPath();
-            $article->type      = 'video';
-            $article->image_url = '/images/uploadImage.jpg'; //默认图
-            $article->video_id  = $video->id;
-            $article->title     = $file->getClientOriginalName(); //新上传的文件直接使用文件的原始名称
-            $article->save();
-
-            //保存视频,简单保存视频不需要更新文章的发布状态
-            $video->saveFile($file, false);
-            return [
-                'video_id'  => $video->id,
-                'video_url' => $video->url(),
-                'image_url' => url('/images/uploadImage.jpg'),
-            ];
+            return $this->storeWithFile($file);
         }
 
-        //腾讯云上传成功后的回调
+        //腾讯云 视频云 web /native sdk 上传成功后的回调
         if ($request->from == 'qcvod') {
             $video = Video::firstOrNew([
                 'qcvod_fileid' => $request->fileId,
             ]);
             $video->user_id = getUserId();
-            $video->path    = get_secure_url($request->videoUrl);
+            $video->path    = get_secure_url($request->videoUrl); //保存https的video cdn 地址
             $video->title   = $request->videoName;
             $video->save();
 
             //调用 vod api , 开始转码，水印，生成截图（非300*200，需要后面UI处理显示效果） ...
             QcloudUtils::simpleProcessFile($request->fileId);
-
-            //TODO:: 保存主封面 save video->cover, then sync to article->image_url
-            //定时任务，pull_events,还没有根据fileid getVideoInfo直接拉取方便，延迟job还未必可靠，schedule:run是个不错的选择
-
             return $video;
         }
 
         return "没有腾讯云视频id，也没有真实上传的视频文件";
+    }
+
+    public function storeWithFile($file)
+    {
+        $hash  = md5_file($file->path());
+        $video = Video::firstOrNew([
+            'hash' => $hash,
+        ]);
+        if ($video->id) {
+            // abort(505,"相同视频已存在");
+        }
+        $video->title = $file->getClientOriginalName();
+        $video->save();
+        //虽然是简单的上传文件但是文章关系还是存了下来
+        $article            = new Article();
+        $article->user_id   = getUserId();
+        $article->video_url = $video->getPath();
+        $article->type      = 'video';
+        $article->image_url = '/images/uploadImage.jpg'; //默认图
+        $article->video_id  = $video->id;
+        $article->title     = $file->getClientOriginalName(); //新上传的文件直接使用文件的原始名称
+        $article->save();
+
+        //保存视频,简单保存视频不需要更新文章的发布状态
+        $video->saveFile($file, false);
+        return [
+            'video_id'  => $video->id,
+            'video_url' => $video->url(),
+            'image_url' => url('/images/uploadImage.jpg'),
+        ];
     }
 
     public function show($id)
--- routes/api/video.php
+++ routes/api/video.php
@@ -4,6 +4,6 @@
 Route::get('/getlatestVideo','Api\VideoController@getLatestVideo');
 
 //視頻列表
-Route::get('videos', 'Api\VideoController@index');
+Route::get('videos', 'Api\VideoController@index'); //旧的api
 Route::get('/video/{id}', 'Api\VideoController@show');
-Route::middleware('auth:api')->post('/video/save', 'Api\VideoController@store');
+Route::middleware('auth:api')->post('/video/save', 'Api\VideoController@store'); //上传视频接口
