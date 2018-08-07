d1adc963e3544a29df6ce7c4a93a4ebe8edc4e06
ivan <ivan@haxibiao.com>
2018-08-07 15:17:43 +0800

    添加page-speed中间件，压缩提升网页传输速度

--- ops/envoy/tasks.php
+++ ops/envoy/tasks.php
@@ -55,7 +55,12 @@ EOT;
 
 $cache_clear = <<<EOT
 composer dump-autoload
+php artisan optimize --force
 php artisan cache:clear
+php artisan view:clear
+php artisan config:clear
+php artisan config:cache
+php artisan route:cache
 EOT;
 
 $copy_ui_build = <<<EOT
