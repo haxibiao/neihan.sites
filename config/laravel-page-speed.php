6c9355c4036d9cd689b77f9edd74bfedad70898c
ivan <ivan@haxibiao.com>
2018-08-07 15:47:45 +0800

    关闭page-speed严肃哦

--- config/laravel-page-speed.php
+++ config/laravel-page-speed.php
@@ -11,7 +11,7 @@ return [
     | You would probably replace that in your local configuration to get a readable output.
     |
     */
-    'enable' => env('LARAVEL_PAGE_SPEED_ENABLE', true),
+    'enable' => env('LARAVEL_PAGE_SPEED_ENABLE', false),
 
     /*
     |--------------------------------------------------------------------------
