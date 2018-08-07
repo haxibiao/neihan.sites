367f2edc3c73fc1f8a0d7ea6f47cb29ce97c4529
czg <caizhigang@haxibiao.com>
2018-08-07 16:02:20 +0800

    PC 修改首页排序算法

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -14,104 +14,7 @@ class IndexController extends Controller
 {
     public function index()
     {
-        $has_follow_articles = false;
-        //get user related categories ..
-        $categorie_ids = [];
-
-        $stick_categories = get_stick_categories();
-        $top_count        = 7 - count($stick_categories);
-        if ($top_count) {
-            foreach ($stick_categories as $stick_category) {
-                $categorie_ids[] = $stick_category->id;
-            }
-        }
-        //已登录，优先显示专注的专题的文章
-        if (Auth::check()) {
-            $user = Auth::user();
-            //get top n user followed categories
-            if ($user->followingCategories()->count() > 6) {
-                $follows = $user->followingCategories()
-                    ->orderBy('id', 'desc')
-                    ->take($top_count)
-                    ->get();
-                $categories = [];
-
-                foreach ($follows as $follow) {
-                    $category        = $follow->followed;
-                    $categories[]    = $category;
-                    $categorie_ids[] = $category->id;
-                }
-                // get user followed categories related articles ...
-                $articles = Article::with('user')->with('category')
-                    ->where('status', '>', 0)
-                    ->where('source_url', '=', '0')
-                    ->whereIn('category_id', $categorie_ids)
-                    ->orderBy('updated_at', 'desc')
-                    ->paginate(10);
-
-                if (!$articles->isEmpty()) {
-                    $has_follow_articles = true;
-                }
-            }
-        }
-
-        //未登录或者未关注任何专题，直接取官方专题和文章
-        if (!$has_follow_articles) {
-            $categories = Category::orderBy('is_official', 'desc')
-                ->where('count', '>=', 0)
-                ->where('status', '>=', 0)
-                ->orderBy('updated_at', 'desc')
-                ->take($top_count)
-                ->get();
-            $articles = Article::with('user')->with('category')
-                ->where('status', '>', 0)
-                ->where('source_url', '=', '0')
-                ->whereIn('category_id', array_merge($categorie_ids, $categories->pluck('id')->toArray()))
-                ->orderBy('updated_at', 'desc')
-                ->paginate(10);
-        }
-
-        //load more articles ...
-        if (request()->ajax() || request('debug')) {
-            foreach ($articles as $article) {
-                $article->fillForJs();
-                $article->time_ago = $article->updatedAt();
-            }
-            return $articles;
-        }
-
-        $categories       = get_top_categoires($categories);
-        $data             = (object) [];
-        $data->categories = $categories;
-
-        //get sticks and filter sticks ....
-        $total        = $articles->total();
-        $sticks       = new Collection(get_stick_articles('发现'));
-        $data->sticks = $sticks;
-        $articles     = $articles->filter(function ($article) use ($sticks) {
-            return !in_array($article->id, $sticks->pluck('id')->toArray());
-        });
-        if (request()->page > 1) {
-            $data->sticks = [];
-        }
-
-        //移动端，用简单的分页样式
-        if (\Agent::isMobile()) {
-            $data->articles = new Paginator(new Collection($articles), 10);
-            $data->articles->hasMorePagesWhen($total > request('page') * 10);
-        } else {
-            $data->articles = new LengthAwarePaginator(new Collection($articles), $total, 10);
-        }
-
-        $data->carousel = get_top_articles();
-
-        //首页推荐视频  TODO  评论时间来排序
-        $data->videoPosts = Article::with('video')->where('type', 'video')
-            ->orderBy('id', 'desc')
-            ->where('status', '>', 0)
-            ->paginate(4);
-        return view('index.index')
-            ->withData($data);
+        return $this->indexRecommendationStrategy();
     }
 
     public function app()
@@ -240,6 +143,110 @@ class IndexController extends Controller
             ->where('status', '>', 0)
             ->paginate(4);
 
+        return view('index.index')
+            ->withData($data);
+    }
+    /**
+     * 首页算法历史版本
+     * @return [type] [description]
+     */
+    public function oldIndexRecommendationStrategy(){
+    	$has_follow_articles = false;
+        //get user related categories ..
+        $categorie_ids = [];
+
+        $stick_categories = get_stick_categories();
+        $top_count        = 7 - count($stick_categories);
+        if ($top_count) {
+            foreach ($stick_categories as $stick_category) {
+                $categorie_ids[] = $stick_category->id;
+            }
+        }
+        //已登录，优先显示专注的专题的文章
+        if (Auth::check()) {
+            $user = Auth::user();
+            //get top n user followed categories
+            if ($user->followingCategories()->count() > 6) {
+                $follows = $user->followingCategories()
+                    ->orderBy('id', 'desc')
+                    ->take($top_count)
+                    ->get();
+                $categories = [];
+
+                foreach ($follows as $follow) {
+                    $category        = $follow->followed;
+                    $categories[]    = $category;
+                    $categorie_ids[] = $category->id;
+                }
+                // get user followed categories related articles ...
+                $articles = Article::with('user')->with('category')
+                    ->where('status', '>', 0)
+                    ->where('source_url', '=', '0')
+                    ->whereIn('category_id', $categorie_ids)
+                    ->orderBy('updated_at', 'desc')
+                    ->paginate(10);
+
+                if (!$articles->isEmpty()) {
+                    $has_follow_articles = true;
+                }
+            }
+        }
+
+        //未登录或者未关注任何专题，直接取官方专题和文章
+        if (!$has_follow_articles) {
+            $categories = Category::orderBy('is_official', 'desc')
+                ->where('count', '>=', 0)
+                ->where('status', '>=', 0)
+                ->orderBy('updated_at', 'desc')
+                ->take($top_count)
+                ->get();
+            $articles = Article::with('user')->with('category')
+                ->where('status', '>', 0)
+                ->where('source_url', '=', '0')
+                ->whereIn('category_id', array_merge($categorie_ids, $categories->pluck('id')->toArray()))
+                ->orderBy('updated_at', 'desc')
+                ->paginate(10);
+        }
+
+        //load more articles ...
+        if (request()->ajax() || request('debug')) {
+            foreach ($articles as $article) {
+                $article->fillForJs();
+                $article->time_ago = $article->updatedAt();
+            }
+            return $articles;
+        }
+
+        $categories       = get_top_categoires($categories);
+        $data             = (object) [];
+        $data->categories = $categories;
+
+        //get sticks and filter sticks ....
+        $total        = $articles->total();
+        $sticks       = new Collection(get_stick_articles('发现'));
+        $data->sticks = $sticks;
+        $articles     = $articles->filter(function ($article) use ($sticks) {
+            return !in_array($article->id, $sticks->pluck('id')->toArray());
+        });
+        if (request()->page > 1) {
+            $data->sticks = [];
+        }
+
+        //移动端，用简单的分页样式
+        if (\Agent::isMobile()) {
+            $data->articles = new Paginator(new Collection($articles), 10);
+            $data->articles->hasMorePagesWhen($total > request('page') * 10);
+        } else {
+            $data->articles = new LengthAwarePaginator(new Collection($articles), $total, 10);
+        }
+
+        $data->carousel = get_top_articles();
+
+        //首页推荐视频  TODO  评论时间来排序
+        $data->videoPosts = Article::with('video')->where('type', 'video')
+            ->orderBy('id', 'desc')
+            ->where('status', '>', 0)
+            ->paginate(4);
         return view('index.index')
             ->withData($data);
     }
