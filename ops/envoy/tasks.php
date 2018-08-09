f40a39caf62508b12979258b0b57e2fd6893280a
ivan <ivan@big>
2018-08-09 09:26:31 +0800

    去掉config:cache, 避免数据库指向缓存住了，文章到其他数据库

--- ops/envoy/tasks.php
+++ ops/envoy/tasks.php
@@ -55,12 +55,9 @@ EOT;
 
 $cache_clear = <<<EOT
 composer dump-autoload
-php artisan optimize --force
 php artisan cache:clear
 php artisan view:clear
 php artisan config:clear
-php artisan config:cache
-php artisan route:cache
 EOT;
 
 $copy_ui_build = <<<EOT
