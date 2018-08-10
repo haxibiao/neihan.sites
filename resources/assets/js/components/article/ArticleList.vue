cc59e6e3def6bdffbc001af2df48aaaa72362f44
luyanyan <luyanyan@haxibiao.com>
2018-08-10 15:21:16 +0800

    fix 首页异步加载部分的简介链接

--- resources/assets/js/components/article/ArticleList.vue
+++ resources/assets/js/components/article/ArticleList.vue
@@ -22,7 +22,7 @@
 		    <a v-if="article.type =='article'" class="title" :target="isDesktop? '_blank' : '_self'" :href="article.url">
 		        <span>{{ article.title }}</span>
 		    </a>
-		    <p class="abstract">{{ article.description?article.description:article.title }}</p>
+		    <a class="abstract":href="article.url">{{ article.description?article.description:article.title }}</a>
 		    <div class="meta">
 		      <a v-if="article.category" class="category" :target="isDesktop? '_blank' : '_self'" :href="'/' + article.category.name_en">
         		<i class="iconfont icon-zhuanti1"></i>
