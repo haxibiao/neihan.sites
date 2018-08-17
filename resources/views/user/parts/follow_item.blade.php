799cb4f1c6fd2e13d1f4d3860dae0fcd49c1e77c
XXM <xiaoxinming@haxibiao.com>
2018-08-17 14:45:57 +0800

    将144id用户还原为296

--- resources/views/user/parts/follow_item.blade.php
+++ resources/views/user/parts/follow_item.blade.php
@@ -1,6 +1,3 @@
-@php
-  $user->count_followings = $user->followingUsers()->count();
-@endphp
 <li class="user-info info-md">
     <a class="avatar" href="/user/{{ $user->id }}"><img src="{{ $user->avatar() }}" alt=""></a>        
 	  
@@ -16,7 +13,7 @@
     </div>
     <div class="info">
       <div class="meta">
-		      <span>关注 {{ $user->count_followings }}</span><span>粉丝 {{ $user->count_follows }}</span><span>文章 {{ $user->count_articles }}</span>
+		      <span>关注 {{ $user->followingUsers()->count() }}</span><span>粉丝 {{ $user->count_follows }}</span><span>文章 {{ $user->count_articles }}</span>
 		    </div>
 		    <div class="meta">
           写了 {{ $user->count_words }} 字，获得了 {{ $user->count_likes }} 个喜欢
