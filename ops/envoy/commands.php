77f324c7f652ed86935a38c082d02f1dfaf423b8
XXM <xiaoxinming@haxibiao.com>
2018-08-17 14:47:51 +0800

    fix user

--- ops/envoy/commands.php
+++ ops/envoy/commands.php
@@ -1,10 +1,9 @@
 <?php
 
 // 临时命令
-
+// php artisan fix:data users 修复上次296用户 id变成144 导致一系列网页错误
 $run_commands = <<<EOT
-
-
 echo "do some fix data ...."
-
+php artisan fix:data users;
+echo "fix success ...."
 EOT;
