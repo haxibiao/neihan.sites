c8e93e9dcf6764923c9b488e8443a357bc8eaf89
XXM <xiaoxinming@haxibiao.com>
2018-08-22 16:38:04 +0800

    修复 上传视频没有标题

--- resources/views/video/show.blade.php
+++ resources/views/video/show.blade.php
@@ -5,7 +5,7 @@
 @extends('layouts.black')
 
 @section('title')
-	{{ empty($article->title) ? $video->title : $article->title }} -{{ empty($article->category)?env('APP_NAME'): $article->category->name}}
+	{{ $article->get_description() }} -{{ empty($article->category)?env('APP_NAME'): $article->category->name}}
 @stop
 
 @push('seo_og_result')
