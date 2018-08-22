875b01e832b19f2a06e3ae1fa2ebaafed7000274
XXM <xiaoxinming@haxibiao.com>
2018-08-22 15:17:08 +0800

    fix 脚本

--- ops/envoy/commands.php
+++ ops/envoy/commands.php
@@ -3,7 +3,5 @@
 // 临时命令
 // php artisan fix:data users 修复上次296用户 id变成144 导致一系列网页错误
 $run_commands = <<<EOT
-echo "do some fix data ...."
-php artisan fix:data visits
-echo "fix success ...."
+php artisan fix:data notifications
 EOT;
