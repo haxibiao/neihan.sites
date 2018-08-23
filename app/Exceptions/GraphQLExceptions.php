5f06d306128d39f9d009c62cae14fc9cd15f224b
czg <caizhigang@haxibiao.com>
2018-08-23 12:26:16 +0800

    GQL 修复GQL异常捕获脚本

--- app/Exceptions/GraphQLExceptions.php
+++ app/Exceptions/GraphQLExceptions.php
@@ -5,15 +5,16 @@ use Bugsnag\BugsnagLaravel\Facades\Bugsnag;
 use Folklore\GraphQL\Error\ValidationError;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Support\Arr;
+use \App\Exceptions\UnregisteredException;
+use Exception; 
 
 class GraphQLExceptions  {
-	//忽略下列异常
+	//不需要通知的异常放在下面
 	protected static $dontReport = [
 		\App\Exceptions\UnregisteredException::class,//用户未登录异常
 	];
 
-	public static function formatError(\Exception $e) {
-		//错误消息体
+	public static function formatError(Exception $e) {
 		$error = [
 			'message' => $e->getMessage(),
 		];
@@ -23,25 +24,24 @@ class GraphQLExceptions  {
 				return $loc->toArray();
 			}, $locations);
 		}
-		//返回异常堆栈信息中的前一个异常
 		$previous = $e->getPrevious();
-		//是否存在校验异常
 		if ($previous && $previous instanceof ValidationError) {
 			$error['validation'] = $previous
 				->getValidatorMessages();
 		} elseif (!($previous && $previous instanceof MessageError)) {
 			if (env('APP_ENV') == 'prod') {
-				if (!self::shouldReport($e)) {
+				if (self::shouldReport($previous)) {
 					\Bugsnag::notifyException($e);
-				}
+				} 
 			}
-		}
+		} 
 		return $error;
 	}
 
-	protected static function shouldReport(\Exception $e) {
-		return is_null(Arr::first(self::$dontReport, function ($type) use ($e) {
-			return $e instanceof $type;
-		}));
-	}
+	protected static function shouldReport(Exception $e)
+    {
+        return is_null(Arr::first(self::$dontReport, function ($type) use ($e) {
+            return $e instanceof $type;
+        }));
+    }
 }
