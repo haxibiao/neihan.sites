721eea6d6a5116b11521835dcad2e8af5a4e56bb
XXM <xiaoxinming@haxibiao.com>
2018-09-04 11:02:26 +0800

    统一官方博客的name_en

--- resources/views/index/parts/special/ainicheng_com.blade.php
+++ resources/views/index/parts/special/ainicheng_com.blade.php
@@ -7,7 +7,7 @@
     </span>
 </a> --}}
 <div class="board-list">
-    <a class="classroom" href="/ainichengguanfang" target="_blank">
+    <a class="classroom" href="/blog" target="_blank">
       <span class="board-title">爱你城官方小课堂<i class="iconfont icon-youbian"></i></span>
       <i class="iconfont icon-diannao board-right"></i>
     </a>
--- resources/views/index/parts/special/dongdianyao_com.blade.php
+++ resources/views/index/parts/special/dongdianyao_com.blade.php
@@ -1,5 +1,5 @@
 <div class="board-list">
-    <a class="classroom" href="/guanfangxiaoketang" target="_blank">
+    <a class="classroom" href="/blog" target="_blank">
       <span class="board-title">{{ env("APP_NAME") }}官方小课堂<i class="iconfont icon-youbian"></i></span>
       <i class="iconfont icon-diannao board-right"></i>
     </a>
