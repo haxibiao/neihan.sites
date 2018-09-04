ae844b24fb46294c67d86eebc7ca725ffe135020
czg <caizhigang@haxibiao.com>
2018-09-04 14:16:43 +0800

    GQL:修复喜欢文章频繁导致冗余数据不一致

--- app/GraphQL/Query/ActionsQuery.php
+++ app/GraphQL/Query/ActionsQuery.php
@@ -7,43 +7,40 @@ use Folklore\GraphQL\Support\Query;
 use GraphQL;
 use GraphQL\Type\Definition\Type;
 
-class ActionsQuery extends Query
-{
-    protected $attributes = [
-        'name' => 'Actions',
-    ];
+class ActionsQuery extends Query {
+	protected $attributes = [
+		'name' => 'Actions',
+	];
 
-    public function type()
-    {
-        return Type::listOf(GraphQL::type('Action'));
-    }
+	public function type() {
+		return Type::listOf(GraphQL::type('Action'));
+	}
 
-    public function args()
-    {
-        return [
-            'user_id' => ['name' => 'user_id', 'type' => Type::int()],
-            'offset'  => ['name' => 'offset', 'type' => Type::int()],
-            'limit'   => ['name' => 'limit', 'type' => Type::int()],
-            // 'filter'  => ['name' => 'filter', 'type' => GraphQL::type('ActionFilter')],
-        ];
-    }
+	public function args() {
+		return [
+			'user_id' => ['name' => 'user_id', 'type' => Type::int()],
+			'offset' => ['name' => 'offset', 'type' => Type::int()],
+			'limit' => ['name' => 'limit', 'type' => Type::int()],
+			// 'filter'  => ['name' => 'filter', 'type' => GraphQL::type('ActionFilter')],
+		];
+	}
 
-    public function resolve($root, $args)
-    {
-        $qb = Action::orderBy('id', 'desc');
+	public function resolve($root, $args) {
+		$qb = Action::orderBy('id', 'desc')
+			->where('actionable_type', '!=', 'favorites');
 
-        if (isset($args['user_id'])) {
-            $qb = $qb->where('user_id', $args['user_id']);
-        }
+		if (isset($args['user_id'])) {
+			$qb = $qb->where('user_id', $args['user_id']);
+		}
 
-        if (isset($args['offset'])) {
-            $qb = $qb->skip($args['offset']);
-        }
-        $limit = 10;
-        if (isset($args['limit'])) {
-            $limit = $args['limit'];
-        }
-        $qb = $qb->take($limit);
-        return $qb->get();
-    }
+		if (isset($args['offset'])) {
+			$qb = $qb->skip($args['offset']);
+		}
+		$limit = 10;
+		if (isset($args['limit'])) {
+			$limit = $args['limit'];
+		}
+		$qb = $qb->take($limit);
+		return $qb->get();
+	}
 }
--- app/Listeners/NewLike.php
+++ app/Listeners/NewLike.php
@@ -3,59 +3,67 @@
 namespace App\Listeners;
 
 use App\Events\LikeWasCreated;
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Cache;
 
-class NewLike
-{
-    /**
-     * Create the event listener.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
-    }
+class NewLike {
+	/**
+	 * Create the event listener.
+	 *
+	 * @return void
+	 */
+	public function __construct() {
+		//
+	}
 
-    /**
-     * Handle the event.
-     * TODO 这个地方处理的时候还存在问题,框架内部自动加载了commentable，user关系
-     * 
-     * @param  LikeWasCreated  $event
-     * @return void
-     */
-    public function handle(LikeWasCreated $event)
-    {
-        $like = $event->like; 
-        $liked_obj = $like->liked; 
-        $target_user = $liked_obj->user;//文章作者或者发布评论的用户
-        $authorizer = getUser();        //当前登录用户
-        $cacheKey = 'user_' . $authorizer->id . '_like_' . $like->liked_id . '_' . $like->liked_type;
-        if($like->liked_type == 'comments'){
-            if (!Cache::get($cacheKey) && ($authorizer->id != $target_user->id)) {
-                $target_user->notify(new \App\Notifications\ArticleLiked( $liked_obj->commentable->id, $authorizer->id, $liked_obj)); 
-                $target_user->forgetUnreads();//清除缓存
-                Cache::put($cacheKey, 1, 60);
-            }
-            $liked_obj->increment('likes');
-        //动态或文章
-        } else if($like->liked_type == 'articles'){ 
+	/**
+	 * Handle the event.
+	 * TODO 这个地方处理的时候还存在问题,框架内部自动加载了commentable，user关系
+	 *
+	 * @param  LikeWasCreated  $event
+	 * @return void
+	 */
+	public function handle(LikeWasCreated $event) {
+		$like = $event->like;
+		$liked_obj = $like->liked;
+		$target_user = $liked_obj->user; //文章作者或者发布评论的用户
+		$authorizer = getUser(); //当前登录用户
+		$cacheKey = 'user_' . $authorizer->id . '_like_' . $like->liked_id . '_' . $like->liked_type;
+		if ($like->liked_type == 'comments') {
+			if (!Cache::get($cacheKey) && ($authorizer->id != $target_user->id)) {
+				$target_user->notify(new \App\Notifications\ArticleLiked($liked_obj->commentable->id, $authorizer->id, $liked_obj));
+				$target_user->forgetUnreads(); //清除缓存
+				Cache::put($cacheKey, 1, 60);
+			}
+			$liked_obj->likes = $liked_obj->likes()
+				->count();
+			////下面代码注释的原因与DestroyedLike类似
+			//$liked_obj->increment('likes');
+			//动态或文章
+		} else if ($like->liked_type == 'articles') {
 
-            if (!Cache::get($cacheKey) && ($authorizer->id != $target_user->id)) {
-                $target_user->notify(new \App\Notifications\ArticleLiked($like->liked_id, $authorizer->id));
-                $target_user->forgetUnreads();//清除缓存
-                Cache::put($cacheKey, 1, 60);
-            }
-            $liked_obj->increment('count_likes');
-            $target_user->increment('count_likes');
-        }
-        //记录操作日志
-        $action = \App\Action::updateOrCreate([ 
-            'user_id'         => $authorizer->id,
-            'actionable_type' => 'likes',
-            'actionable_id'   => $like->id,
-        ]);
-    }
+			if (!Cache::get($cacheKey) && ($authorizer->id != $target_user->id)) {
+				$target_user->notify(new \App\Notifications\ArticleLiked($like->liked_id, $authorizer->id));
+				$target_user->forgetUnreads(); //清除缓存
+				Cache::put($cacheKey, 1, 60);
+			}
+
+			$target_user = $liked_obj->user;
+			$liked_obj->count_likes = $liked_obj
+				->likes()
+				->count();
+			$target_user->count_likes = $target_user
+				->likes()
+				->count();
+
+			//下面代码注释的原因与DestroyedLike类似
+			//$liked_obj->increment('count_likes');
+			//$target_user->increment('count_likes');
+		}
+		//记录操作日志
+		$action = \App\Action::updateOrCreate([
+			'user_id' => $authorizer->id,
+			'actionable_type' => 'likes',
+			'actionable_id' => $like->id,
+		]);
+	}
 }
