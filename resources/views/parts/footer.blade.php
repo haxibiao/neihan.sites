db35e056c74c9f38a3f7f486dfc097de402de83f
XXM <xiaoxinming@haxibiao.com>
2018-09-04 11:13:56 +0800

    设置底部的一些固定链接

--- resources/views/parts/footer.blade.php
+++ resources/views/parts/footer.blade.php
@@ -1,27 +1,26 @@
 <!-- 网站底部 -->
 <footer id="footer">
     <div>
-        <a target="_blank" href="/">关于{{ config('app.name') }}</a><em> · </em>
-        <a target="_blank" href="">联系我们</a><em> · </em>
-        <a target="_blank" href="">加入我们</a><em> · </em>
+        <a target="_blank" href="/article/guanyuwomen">关于{{ config('app.name') }}</a><em> · </em>
+        <a target="_blank" href="/article/lianxiwomen">联系我们</a><em> · </em>
         <a target="_blank" href="">{{ config('app.name') }}出版</a><em> · </em>
         <a target="_blank" href="/sitemap" title="站点地图">站点地图</a><em> · </em>
-        <a target="_blank" href="{{ config('editor.help') }}">帮助中心</a><em> · </em>
+        <a target="_blank" href="/article/bangzhuzhongxin">帮助中心</a><em> · </em>
         <a target="_blank" href="/user">全部作者</a>
         
     </div>
     <div class="icp">
         <div>
-            ©2012-2018 {{ env('COMPANY') }} / {{ config('app.name') }} / {{ env('ICPB') }}/{{ config('editor.appId.recordId') }}
+            ©2017-2018 {{ env('COMPANY') }} / {{ config('app.name') }} / {{ env('ICPB') }}
             <a target="_blank" href="" alt="Smrz">
-                <img src="/images/smrz.png" alt="">
-            </a> 
+                <img src="/images/smrz.png" alt="实名认证">
+            </a> / 
             <a target="_blank" href="" alt="Zggsrz">
-                <img src="/images/yyzz.png" alt="">
+                <img src="/images/yyzz.png" alt="电子安全监督">
             </a> 
         </div>
         公安备案号:<a target="_blank" href="">{{ env('ICPGA') }} / </a>
-        举报电话：028-69514351
+        举报电话：028-69514351, 联系网站管理 <a href="mailto:{{ "admin@".get_domain() }}">{{ "admin@".get_domain() }}</a>
     </div>
     <div>
         友情链接: @include('parts.friend_links') <a target="_blank" href="/">{{ config('app.name') }}</a>
