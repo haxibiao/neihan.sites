244126c28fa83bf828dd7c979e77a7e6473341de
XXM <xiaoxinming@haxibiao.com>
2018-09-13 12:29:28 +0800

    更新了 web端下载app的二维码

--- resources/views/index/parts/download_app.blade.php
+++ resources/views/index/parts/download_app.blade.php
@@ -1,7 +1,7 @@
 <div class="download-app">
 <a target="{{ \Agent::isDeskTop()? '_blank':'_self' }}" class="app" href="app" data-toggle="popover" data-placement="top" data-html="true" data-trigger="hover" 
-data-content="<img src=/qrcode/{{ get_domain() }}.jpg>">
-  <img src="/qrcode/{{ get_domain() }}.jpg" alt="下载{{ config('app.name') }}手机App">
+data-content="<img src=/qrcode/{{ get_domain() }}.png>">
+  <img src="/qrcode/{{ get_domain() }}.png" alt="下载{{ config('app.name') }}手机App">
   <div class="app-info">
     <div class="down">下载{{ config('app.name') }}手机App <i class="iconfont icon-youbian"></i></div>
     <div class="describe">随时随地发现和创作内容</div>
