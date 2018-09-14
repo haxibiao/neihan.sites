fcd74e179fde985b3c40dbbc7d8e73320121f59e
ivan <ivan@haxibiao.com>
2018-09-14 11:40:58 +0800

    精简codesync脚本

--- codesync.sh
+++ codesync.sh
@@ -5,30 +5,29 @@ cd ../$1.com/
 sudo chmod -R 777 .
 git config core.filemode false
 
-echo 'sync code ...'
+echo 'sync laravel code ...'
 sudo /bin/cp -rf /data/www/ainicheng.com/app /data/www/$1.com/
 sudo /bin/cp -rf /data/www/ainicheng.com/config /data/www/$1.com/
 sudo /bin/cp -rf /data/www/ainicheng.com/resources/views /data/www/$1.com/resources/
 sudo /bin/cp -rf /data/www/ainicheng.com/resources/assets/js /data/www/$1.com/resources/assets/
 sudo /bin/cp -rf /data/www/ainicheng.com/routes /data/www/$1.com/
 
-echo "sync ops"
+echo "sync 运维脚本(ops)"
 sudo /bin/cp -rf /data/www/ainicheng.com/ops/envoy/tasks.php /data/www/$1.com/ops/envoy/
 sudo /bin/cp -rf /data/www/ainicheng.com/Envoy.blade.php /data/www/$1.com/
 
-echo "sync composer, package.json"
-sudo /bin/cp -rf /data/www/ainicheng.com/composer.* /data/www/$1.com/
-sudo /bin/cp -rf /data/www/ainicheng.com/package.* /data/www/$1.com/
-
-echo "sync sass ..."
+echo "sync 前端scss js(vue) ..."
+sudo /bin/cp -rf /data/www/ainicheng.com/resources/assets/js /data/www/$1.com/resources/assets/
 sudo /bin/cp -rf /data/www/ainicheng.com/resources/assets/sass /data/www/$1.com/resources/assets/
 git checkout resources/assets/sass/_theme.scss
 
-echo 'sync migrations ...'
+echo 'sync 数据库迁移 ...'
 rm -rf /data/www/$1.com/database/migrations/*
 sudo /bin/cp -rf /data/www/ainicheng.com/database/migrations /data/www/$1.com/database/
 
-echo 'sync webpack.mix.js ...'
+echo "sync php js 的包管理依赖"
+sudo /bin/cp -rf /data/www/ainicheng.com/composer.* /data/www/$1.com/
+sudo /bin/cp -rf /data/www/ainicheng.com/package.* /data/www/$1.com/
 sudo /bin/cp -rf /data/www/ainicheng.com/webpack.mix.js /data/www/$1.com/
 
 # echo 'sync assets ...'
