7c925e3d4e629b40a45627a368a6c3b1d713e50b
czg <czg@haxibiao.com>
2018-09-16 12:57:23 +0800

    add run cmds.sh

---
+++ cmds.sh
@@ -0,0 +1,28 @@
+#!bin/bash
+
+cd ../dongmeiwei.com
+envoy run cmds
+
+cd ../dongdianyao.com
+envoy run cmds
+
+cd ../dongdianyi.com
+envoy run cmds
+
+cd ../qunyige.com
+envoy run cmds
+
+cd ../youjianqi.com
+envoy run cmds
+
+cd ../dianmoge.com
+envoy run cmds
+
+cd ../jucheshe.com
+envoy run cmds
+
+cd ../youwangfa.com
+envoy run cmds
+
+cd ../didilinju.com
+envoy run cmds
