1d05445820fd05552e37c3489f82392e618f6800
czg <caizhigang@haxibiao.com>
2018-10-17 09:32:36 +0800

    fix(index):修复首页有测item个性化显示问题

--- resources/views/index/parts/special/dongdianyi_com.blade.php
+++ resources/views/index/parts/special/dongdianyi_com.blade.php
@@ -1,6 +1,6 @@
 <div class="board-list">
     <a class="classroom" href="/blog" target="_blank">
-      <span class="board-title">{{ env("APP_NAME") }}官方小课堂<i class="iconfont icon-youbian"></i></span>
+      <span class="board-title">{{ config('app.name_cn') }}官方小课堂<i class="iconfont icon-youbian"></i></span>
       <i class="iconfont icon-diannao board-right"></i>
     </a>
 </div>
