66bf8187e0b9010c2c566c8bb5e20a03f5993bbe
XXM <xiaoxinming@haxibiao.com>
2018-10-26 21:51:02 +0800

    修复付费问答Transaction Not found

--- app/Jobs/BonusAnswers.php
+++ app/Jobs/BonusAnswers.php
@@ -3,6 +3,7 @@
 namespace App\Jobs;
 
 use App\Question;
+use App\Transaction;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
