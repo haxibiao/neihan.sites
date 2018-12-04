cc15fa5e3c89b8aebfebb16729ba229a7b58a25c
XXM <xiaoxinming@haxibiao.com>
2018-12-04 14:35:19 +0800

    更新 config/matomo

--- config/matomo.php
+++ config/matomo.php
@@ -4,13 +4,14 @@
  * 对应matomo SiteId
  */
 return [
-	'site'	=> [
-		'ainicheng.com'	=>	1,
-		'dianmoge.com'	=>	2,
-		'qunyige.com'	=>	3,
-		'dongmeiwei.com'	=>	4,
-		'dongdianyi.com'	=>	5,
-		'youjianqi.com'	=>	6,
-	],
-	'matomo'	=> 'https://haxibiao.com/matomo',
+    'site'   => [
+        'ainicheng.com'   => 1,
+        'dianmoge.com'    => 2,
+        'dongmeiwei.com'  => 3,
+        'dongdianyi.com'  => 4,
+        'qunyige.com'     => 5,
+        'youjianqi.com'   => 6,
+        'dongdianyao.com' => 7,
+    ],
+    'matomo' => 'https://haxibiao.com/matomo',
 ];
