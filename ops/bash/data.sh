048bb97296cca08e5e2d8e2398b9a3dba9134dad
liguangyun <liguangyun@haxibiao.com>
2019-12-24 18:12:32 +0800

    sync dianmoge code

--- ops/bash/data.sh
+++ ops/bash/data.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+scope=no
 
+seeder="seed"
 
 #有数据更新的时候，填写下面的 artisan commands 执行命令
 
+if [ $scope = $seeder ];
+then
 echo "修复数据..."
-
-php artisan fix:data articles
-php artisan fix:data articleReviewId
+php artisan migrate --seed
+fi
