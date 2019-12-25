925aa9bb1b91f7cc9523c3933f5cacbda43fda9a
liguangyun <liguangyun@haxibiao.com>
2019-12-25 14:48:30 +0800

    ainicheng.com sync dianmoge code

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,5 @@
 <?php
 
-use FunctionSwitchSeeder;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
--- routes/api/app.php
+++ routes/api/app.php
@@ -6,3 +6,10 @@ Route::get('login', 'Api\UserController@login');
 //APP注册
 Route::get('register', 'Api\UserController@register'); //测试用
 Route::post('register', 'Api\UserController@register');
+
+//广告的配置(方便激励视频每看一次更新)
+Route::get('/ad-config', 'Api\AppController@adConfig');
+//app功能开关(含广告配置)
+Route::get('/app-config', 'Api\AppController@index');
+//app版本管理
+Route::any('/app-version', 'Api\AppController@version');
