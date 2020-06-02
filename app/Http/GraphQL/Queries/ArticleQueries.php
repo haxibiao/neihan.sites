6a0dc2eb4ac2b1b4ebc2d47619d3fa4e6a8744e0
吴秀璋 <wuxiuzhang@haxibiao.com>
2020-06-02 11:17:57 +0800

    YJQ-210  别人的个人主页只展示已收录的动态

--- app/Http/GraphQL/Queries/ArticleQueries.php
+++ app/Http/GraphQL/Queries/ArticleQueries.php
@@ -12,10 +12,10 @@ class ArticleQueries
 
         //TODO 重构并迁移到 ArticleRepo 中
         //排除用户拉黑（屏蔽）的用户发布的视频,排除拉黑（不感兴趣）的动态
-        $userBlockId = [];
-        $articleBlockId = [];  
+        $userBlockId    = [];
+        $articleBlockId = [];
         if ($user = checkUser()) {
-            $userBlockId = \App\UserBlock::select('user_block_id')->whereNotNull('user_block_id')->where('user_id', $user->id)->get();
+            $userBlockId    = \App\UserBlock::select('user_block_id')->whereNotNull('user_block_id')->where('user_id', $user->id)->get();
             $articleBlockId = \App\UserBlock::select('article_block_id')->whereNotNull('article_block_id')->where('user_id', $user->id)->get();
         }
 
@@ -36,6 +36,9 @@ class ArticleQueries
 
         if ($args['status'] != 10) {
             return $query->where('status', $args['status'])->whereNotNull('cover_path');
+        }
+        if (!isset($args['user_id'])) {
+            $query->where('submit', 1);
         }
         $query->when(isset($args['user_id']), function ($q) use ($args) {
             return $q->where('user_id', $args['user_id']);
