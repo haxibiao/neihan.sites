6e9edf2c9a7dab0108ddbf1193e4abca1969e1fe
吴秀璋 <wuxiuzhang@haxibiao.com>
2020-06-24 16:04:36 +0800

    YJQ-225 修复钱包页一直loading

--- app/Traits/WalletAttrs.php
+++ app/Traits/WalletAttrs.php
@@ -2,7 +2,7 @@
 
 namespace App\Traits;
 
-use haxibiao\helpers\Pay\PayUtils;
+use haxibiao\helpers\PayUtils;
 use App\Withdraw;
 
 trait WalletAttrs
--- app/Traits/WithdrawRepo.php
+++ app/Traits/WithdrawRepo.php
@@ -2,7 +2,7 @@
 
 namespace App\Traits;
 
-use haxibiao\helpers\Pay\PayUtils;
+use haxibiao\helpers\PayUtils;
 use App\Transaction;
 use App\User;
 use App\Withdraw;
