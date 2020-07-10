1fcfb9cebdbbd109835a0ae866d8589518413a78
吴秀璋 <wuxiuzhang@haxibiao.com>
2020-07-10 10:59:41 +0800

    up...

--- app/Http/GraphQL/Queries/ArticleQueries.php
+++ app/Http/GraphQL/Queries/ArticleQueries.php
@@ -20,7 +20,7 @@ class ArticleQueries
         }
 
         $query = Article::withoutGlobalScope(ArticleSubmitScope::class)
-            ->whereNotNull("user_id")
+            ->has("user")
             ->whereIn('type', ['video', 'post', 'issue'])
             ->orderBy('id', 'desc');
 
