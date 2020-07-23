672c576d1b07cd6ea58eab45ae770a02be44480f
吴秀璋 <wuxiuzhang@haxibiao.com>
2020-07-23 11:10:38 +0800

    up...

--- app/Category.php
+++ app/Category.php
@@ -3,9 +3,11 @@
 namespace App;
 
 use App\Traits\CategoryRepo;
+use App\Traits\CategoryResolvers;
 use  Haxibiao\Question\Category as BaseCategory;
 
 class Category extends BaseCategory
 {
     use CategoryRepo;
+    use CategoryResolvers;
 }
