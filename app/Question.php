3c7c1481950019cce577c410701774d3a11c8232
吴秀璋 <wuxiuzhang@haxibiao.com>
2020-07-24 22:14:54 +0800

    每天最多能答50道题

--- app/Question.php
+++ app/Question.php
@@ -8,7 +8,7 @@ use  Haxibiao\Question\Question as BaseQuestion;
 class Question extends BaseQuestion
 {
 
-    const MAX_ANSWER = 5;
+    const MAX_ANSWER = 50;
 
     public function resolveCanAnswer($root, array $args, $context, $info)
     {
