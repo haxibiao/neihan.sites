f22e866698f6ae0faeb025dfaa7c2bc53cdb00ce
吴秀璋 <wuxiuzhang@haxibiao.com>
2020-07-24 14:50:59 +0800

    提供是否可以答题的gql接口

--- app/Question.php
+++ app/Question.php
@@ -2,9 +2,19 @@
 
 namespace App;
 
+use App\Exceptions\GQLException;
 use  Haxibiao\Question\Question as BaseQuestion;
 
 class Question extends BaseQuestion
 {
-    
+
+    const MAX_ANSWER = 30;
+    public function resolveCanAnswer($root, array $args, $context, $info)
+    {
+        if ($user = checkUser()) {
+            if ($user->answer->where("created_at", ">=", now()->toDateString())->count() > Question::MAX_ANSWER) {
+                throw new GQLException("今天答题超过上限啦~，明天再来吧 ->_<-");
+            }
+        }
+    }
 }
