5d5eaff7138b42d82730ea674c6e3871dc67cd66
吴秀璋 <wuxiuzhang@haxibiao.com>
2020-07-24 14:57:07 +0800

    up...

--- app/Question.php
+++ app/Question.php
@@ -9,12 +9,14 @@ class Question extends BaseQuestion
 {
 
     const MAX_ANSWER = 30;
+
     public function resolveCanAnswer($root, array $args, $context, $info)
     {
         if ($user = checkUser()) {
             if ($user->answer->where("created_at", ">=", now()->toDateString())->count() > Question::MAX_ANSWER) {
                 throw new GQLException("今天答题超过上限啦~，明天再来吧 ->_<-");
             }
+            return 1;
         }
     }
 }
