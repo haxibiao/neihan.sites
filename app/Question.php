149f4d295246fe393b2d1ddfb88c08f934818e79
吴秀璋 <wuxiuzhang@haxibiao.com>
2020-08-03 11:44:15 +0800

    答题up。。。

--- app/Question.php
+++ app/Question.php
@@ -7,16 +7,4 @@ use  Haxibiao\Question\Question as BaseQuestion;
 
 class Question extends BaseQuestion
 {
-
-    const MAX_ANSWER = 50;
-
-    public function resolveCanAnswer($root, array $args, $context, $info)
-    {
-        if ($user = checkUser()) {
-            if ($user->answers->where("created_at", ">=", now()->toDateString())->count() > Question::MAX_ANSWER) {
-                throw new GQLException("今天答题超过上限啦~，明天再来吧 ->_<-");
-            }
-            return 1;
-        }
-    }
 }
