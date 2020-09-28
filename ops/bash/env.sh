9311dc756e8ed9c5430d8bc8150c0d3ad6b3ceab
吴秀璋 <wuxiuzhang@haxibiao.com>
2020-09-28 15:20:59 +0800

    ops up...

--- ops/bash/env.sh
+++ ops/bash/env.sh
@@ -7,7 +7,7 @@ db_host="ngz009" #默认线上数据库
 if [ ! -z $1 ];then
     branch=$1
     #hotfix和线上一样的数据库
-    if [ "$branch" != "hotfix" ]; then
+    if [ "$branch" !="develop" ]; then
         db="ainicheng_staging" #其他环境的都用测试数据库吧...
         db_host="localhost" #非线上环境数据库
     fi
