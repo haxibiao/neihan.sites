7fccb693eef1ca178a2c0b4c20d0ce8f97201682
吴秀璋 <wuxiuzhang@haxibiao.com>
2020-09-30 12:07:48 +0800

    添加中秋国庆点赞任务

--- app/Observers/LikeObserver.php
+++ app/Observers/LikeObserver.php
@@ -2,6 +2,7 @@
 
 namespace App\Observers;
 
+use App\Collection;
 use App\Contribute;
 use App\Events\NewLike;
 use App\Like;
@@ -18,9 +19,16 @@ class LikeObserver
         //检查点赞任务是否完成了
         $user->reviewTasksByClass(get_class($like));
 
-        app_track_event('用户','点赞');
-        event(new NewLike($like));
+        if ($like->likable instanceof \App\Collection) {
+            if ($like->likable->name == "我爱中国") {
+                \App\Task::refreshTask($user, "国庆活动");
+            } else if ($like->likable->name == "中秋故事汇") {
+                \App\Task::refreshTask($user, "中秋活动");
+            }
+        }
 
+        app_track_event('用户', '点赞');
+        event(new NewLike($like));
     }
 
     public function deleted(Like $like)
