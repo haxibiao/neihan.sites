ce6a6cd11d0787cbe792470b15983bccbc1c50c6
吴秀璋 <wuxiuzhang@haxibiao.com>
2020-09-30 17:18:39 +0800

    国庆中秋任务进度检查up...

--- app/Observers/LikeObserver.php
+++ app/Observers/LikeObserver.php
@@ -20,7 +20,7 @@ class LikeObserver
         $user->reviewTasksByClass(get_class($like));
 
         if ($like->likable instanceof \App\Collection) {
-            if ($like->likable->name == "我爱中国") {
+            if ($like->likable->name == "一起看阅兵") {
                 \App\Task::refreshTask($user, "国庆活动");
             } else if ($like->likable->name == "中秋故事汇") {
                 \App\Task::refreshTask($user, "中秋活动");
