a7d4231ae408618b1450748d06c7746ab42b04af
吴秀璋 <wuxiuzhang@haxibiao.com>
2020-10-13 10:22:49 +0800

    迁移文件up。。。。

--- database/migrations/2019_09_29_194907_create_imageables2_table.php
+++ database/migrations/2019_09_29_194907_create_imageables2_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateImageablesTable extends Migration
+class CreateImageables2Table extends Migration
 {
     /**
      * Run the migrations.
