cb813f967f220e7d5362b2dd4b1cebec537210b6
吴秀璋 <wuxiuzhang@haxibiao.com>
2020-10-14 10:38:09 +0800

    fixed:web个人页报错

--- resources/views/user/parts/follow_item.blade.php
+++ resources/views/user/parts/follow_item.blade.php
@@ -1,22 +1,20 @@
 <li class="user-info info-md">
-    <a class="avatar" href="/user/{{ $user->id }}"><img src="{{ $user->avatarUrl }}" alt=""></a>        
-	  
-    <follow 
-      type="users" 
-      id="{{ $user->id }}" 
-      user-id="{{ user_id() }}" 
-      followed="{{ is_follow('users', $user->id) }}">
-    </follow>
+  if($user){
+  <a class="avatar" href="/user/{{ $user->id }}"><img src="{{ $user->avatarUrl }}" alt=""></a>
 
-    <div class="title">
-      <a href="/user/{{ $user->id }}" class="name">{{ $user->name }}</a>
+  <follow type="users" id="{{ $user->id }}" user-id="{{ user_id() }}" followed="{{ is_follow('users', $user->id) }}">
+  </follow>
+
+  <div class="title">
+    <a href="/user/{{ $user->id }}" class="name">{{ $user->name }}</a>
+  </div>
+  <div class="info">
+    <div class="meta">
+      <span>关注 {{ $user->followingUsers()->count() }}</span><span>粉丝 {{ $user->count_follows }}</span><span>文章 {{ $user->count_articles }}</span>
     </div>
-    <div class="info">
-      <div class="meta">
-		      <span>关注 {{ $user->followingUsers()->count() }}</span><span>粉丝 {{ $user->count_follows }}</span><span>文章 {{ $user->count_articles }}</span>
-		    </div>
-		    <div class="meta">
-          写了 {{ $user->count_words }} 字，获得了 {{ $user->count_likes }} 个喜欢
-        </div>
+    <div class="meta">
+      写了 {{ $user->count_words }} 字，获得了 {{ $user->count_likes }} 个喜欢
     </div>
+  </div>
+  }
 </li>
