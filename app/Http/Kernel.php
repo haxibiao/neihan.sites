c4982642dd6f723508ff2b19d1a680b62c752091
吴秀璋 <wuxiuzhang@haxibiao.com>
2020-10-30 15:18:54 +0800

    ANC-17 fixed:看完ad广告没有奖励

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -52,6 +52,7 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $routeMiddleware = [
+           'graphql'       => \App\Http\Middleware\ThrottleGraphQL::class,
         'auth'         => \Illuminate\Auth\Middleware\Authenticate::class,
         'auth.basic'   => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'auth.editor'  => \App\Http\Middleware\AuthEditor::class,
