464c978da675ec4ce10284a817bacffd9e6a9f02
ivan <ivan@haxibiao.com>
2020-11-19 11:56:50 +0800

    测试单个google analytics

--- resources/views/parts/js_for_footer.blade.php
+++ resources/views/parts/js_for_footer.blade.php
@@ -1,49 +1,72 @@
 {{-- 腾讯统计 --}}
 <script>
-var _mtac = {};
-(function() {
-	var mta = document.createElement("script");
-	mta.src = "//pingjs.qq.com/h5/stats.js?v2.0.4";
-	mta.setAttribute("name", "MTAH5");
-	mta.setAttribute("sid", "500733348");
-	var s = document.getElementsByTagName("script")[0];
-	s.parentNode.insertBefore(mta, s);
-})();
+    var _mtac = {};
+    (function() {
+        var mta = document.createElement("script");
+        mta.src = "//pingjs.qq.com/h5/stats.js?v2.0.4";
+        mta.setAttribute("name", "MTAH5");
+        mta.setAttribute("sid", "500733348");
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(mta, s);
+    })();
+
 </script>
 
 {{-- 百度推送 --}}
 <script>
-    (function(){
+    (function() {
         var canonicalURL, curProtocol;
         //Get the <link> tag
-        var x=document.getElementsByTagName("link");
+        var x = document.getElementsByTagName("link");
         //Find the last canonical URL
-        if(x.length > 0){
-            for (i=0;i<x.length;i++){
-                if(x[i].rel.toLowerCase() == 'canonical' && x[i].href){
-                    canonicalURL=x[i].href;
+        if (x.length > 0) {
+            for (i = 0; i < x.length; i++) {
+                if (x[i].rel.toLowerCase() == 'canonical' && x[i].href) {
+                    canonicalURL = x[i].href;
                 }
             }
         }
         //Get protocol
-        if (!canonicalURL){
+        if (!canonicalURL) {
             curProtocol = window.location.protocol.split(':')[0];
-        }
-        else{
+        } else {
             curProtocol = canonicalURL.split(':')[0];
         }
         //Get current URL if the canonical URL does not exist
         if (!canonicalURL) canonicalURL = window.location.href;
         //Assign script content. Replace current URL with the canonical URL
-        !function(){var e=/([http|https]:\/\/[a-zA-Z0-9\_\.]+\.baidu\.com)/gi,r=canonicalURL,t=document.referrer;if(!e.test(r)){var n=(String(curProtocol).toLowerCase() === 'https')?"https://sp0.baidu.com/9_Q4simg2RQJ8t7jm9iCKT-xh_/s.gif":"//api.share.baidu.com/s.gif";t?(n+="?r="+encodeURIComponent(document.referrer),r&&(n+="&l="+r)):r&&(n+="?l="+r);var i=new Image;i.src=n}}(window);})();
+        ! function() {
+            var e = /([http|https]:\/\/[a-zA-Z0-9\_\.]+\.baidu\.com)/gi,
+                r = canonicalURL,
+                t = document.referrer;
+            if (!e.test(r)) {
+                var n = (String(curProtocol).toLowerCase() === 'https') ?
+                    "https://sp0.baidu.com/9_Q4simg2RQJ8t7jm9iCKT-xh_/s.gif" : "//api.share.baidu.com/s.gif";
+                t ? (n += "?r=" + encodeURIComponent(document.referrer), r && (n += "&l=" + r)) : r && (n += "?l=" +
+                    r);
+                var i = new Image;
+                i.src = n
+            }
+        }(window);
+    })();
+
 </script>
 
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-W72CHJT74V"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+@php
+{{-- $ga_id = "G-W72CHJT74V";//内涵电影 --}}
+$ga_id = "G-CRK4B4W5R4";//疯狂美剧
+@endphp
 
-  gtag('config', 'G-W72CHJT74V');
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id={{ $ga_id }}"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', '{{ $ga_id }}');
+
 </script>
