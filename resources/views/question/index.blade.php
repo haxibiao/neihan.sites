5524fdba2b5d5081249e47c3cc627c461bad20e4
曾达威 <zengdawei@haxibiao.com>
2020-11-24 14:34:20 +0800

    NHDY-131 修复网页标题不对

--- resources/views/question/index.blade.php
+++ resources/views/question/index.blade.php
@@ -1,36 +1,36 @@
 @extends('layouts.app')
 
-@section('title')问答 - {{ config("app.name_cn") }} @stop
-@section('description') {{ get_seo_description()  }} @stop
+@section('title')问答 - {{ neihan_sites_domains() }} @stop
+@section('description') {{ get_seo_description() }} @stop
 @section('keywords') {{ get_seo_keywords() }} @stop
 
 @push('section')
- <div id="questions">
-    {{-- 问答专题 --}}
-    <div class="questions-list">
-      <div class="container">
-        <div class="row">
-          <div class="col-xs-12">
-            @include('question.parts.recommend_categories')
-          </div>
+    <div id="questions">
+        {{-- 问答专题 --}}
+        <div class="questions-list">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12">
+                        @include('question.parts.recommend_categories')
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
+        <div class="container">
+            <div class="wrap row">
+                <div class="main sm-left">
+                    @each('question.parts.question_item', $questions ,'question')
+                    <div class="pager">
+                        {!! $questions->appends(['cid' => request('cid')])->links() !!}
+                    </div>
+                </div>
+                <div class="aside sm-right hidden-xs">
+                    {{-- @include('topic.parts.carousel') --}}
+                    @include('question.parts.hot_questions')
+                    @include('question.parts.contact')
+                </div>
+            </div>
+        </div>
+        <modal-ask-question></modal-ask-question>
     </div>
-   <div class="container">
-     <div class="wrap row">
-       <div class="main sm-left">
-         @each('question.parts.question_item',  $questions ,'question')
-         <div class="pager">
-           {!! $questions->appends(['cid' => request('cid')])->links() !!}
-         </div>
-       </div>
-       <div class="aside sm-right hidden-xs">
-         {{-- @include('topic.parts.carousel') --}}
-         @include('question.parts.hot_questions')
-         @include('question.parts.contact')
-       </div>
-     </div>
-   </div>
-  <modal-ask-question></modal-ask-question>
- </div>
 @endpush
