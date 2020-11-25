e7fac9d5f227ce7b9818d20addb1e447ee39add7
曾达威 <zengdawei@haxibiao.com>
2020-11-25 11:34:16 +0800

    fix: 专题 500

--- resources/views/category/parts/related.blade.php
+++ resources/views/category/parts/related.blade.php
@@ -1,13 +1,13 @@
 <div class="atten-people">
 	<p class="plate-title">相关专题</p>
     <ul>
-    	@foreach($data['related_category'] as $category)         
+    	{{-- @foreach($data['related_category'] as $category)         
 	    	 <li class="single-media">
 	    	 	<a href="/category/{{ $category->id }}" class="avatar-category">
 		    	 	<img src="{{ $category->logo ?: 'images/category.logo.jpg' }}" alt="{{ $category->name }}">
 		    	 </a>
 	    	 	<a href="/category/{{ $category->id }}" class="info">{{ $category->name }}</a>
 	    	</li>
-    	@endforeach
+    	@endforeach --}}
     </ul>
 </div>
