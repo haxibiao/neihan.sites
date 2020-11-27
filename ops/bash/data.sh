330fbb68788e61f42a31441f29c3120e57f8958d
ivan <ivan@haxibiao.com>
2020-11-27 12:48:52 +0800

    fix ops

--- ops/bash/data.sh
+++ ops/bash/data.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 php artisan migrate
+
 #有数据更新的时候，填写下面的 artisan commands 执行命令
 
 # echo "修复数据(看来每次需要tag修数据时，都需要记得清理这里上一次的代码)..."
--- ops/bash/pull.sh
+++ ops/bash/pull.sh
@@ -8,4 +8,4 @@ echo "更新子模块git代码..."
 git submodule init && git submodule update && git pull --recurse-submodules
 
 echo "安装依赖..."
-composer install
+composer install -o
