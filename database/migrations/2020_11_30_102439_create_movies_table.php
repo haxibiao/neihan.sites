78d3c33125ab9f092d41459e397cb5c9b3f6cee4
张志明 <zhangzhiming@haxifang.com>
2020-12-01 10:11:50 +0800

    注释已经存在table的migration

--- database/migrations/2020_11_30_102439_create_movies_table.php
+++ database/migrations/2020_11_30_102439_create_movies_table.php
@@ -13,21 +13,22 @@ class CreateMoviesTable extends Migration
      */
     public function up()
     {
-        Schema::create('movies', function (Blueprint $table) {
-            $table->id();
-            $table->string('name', 100)->index()->comment("电影名");
-            $table->string('introduction', 255)->index()->comment("简介");
-            $table->string('cover', 200)->index()->comment("封面");
-            $table->string('producer', 100)->index()->comment("导演");
-            $table->string('year', 100)->index()->comment("年份");
-            $table->string('type', 100)->index()->comment("分类");
-            $table->string('style', 100)->index()->comment("风格");
-            $table->string('region', 100)->index()->comment("地区");
-            $table->integer('count_series')->default(0)->comment("总集数");
-            $table->string('actors', 100)->index()->comment("演员");
-            $table->json('data')->comment('剧集播放数据');
-            $table->timestamps();
-        });
+
+        // Schema::create('movies', function (Blueprint $table) {
+        //     $table->id();
+        //     $table->string('name', 100)->index()->comment("电影名");
+        //     $table->string('introduction', 255)->index()->comment("简介");
+        //     $table->string('cover', 200)->index()->comment("封面");
+        //     $table->string('producer', 100)->index()->comment("导演");
+        //     $table->string('year', 100)->index()->comment("年份");
+        //     $table->string('type', 100)->index()->comment("分类");
+        //     $table->string('style', 100)->index()->comment("风格");
+        //     $table->string('region', 100)->index()->comment("地区");
+        //     $table->integer('count_series')->default(0)->comment("总集数");
+        //     $table->string('actors', 100)->index()->comment("演员");
+        //     $table->json('data')->comment('剧集播放数据');
+        //     $table->timestamps();
+        // });
     }
 
     /**
