12ec7966f3b66b84b630fa479c20d20e86b1060d
张博 <zhangbo@haxifang.cn>
2020-12-11 14:35:51 +0800

    修复浏览记录

--- resources/views/layouts/movie/header.blade.php
+++ resources/views/layouts/movie/header.blade.php
@@ -22,41 +22,20 @@
                 {{-- <li><a href="#" title="留言反馈"><i class="iconfont icon-comments-fill"></i></a></li> --}}
                 @if (Auth::check())
                     <li title="播放记录" dropdown-target=".play-history" dropdown-toggle="hover">
-                        <a href="javascript:;">
+                        <a href="/movie/collection?type=history">
                             <i class="iconfont icon-clock-fill"></i>
                         </a>
-                        <!-- <div class="dropdown-box play-history">
-                            <div class="history-box clearfix">
-                                <div class="ht-movie_list">
-                                    <div class="video_headline">播放记录</div>
-                                    @php
-                                    $historyMovies
-                                    =Auth::user()->movieHistory()->orderByDesc('updated_at')->take(10)->get();
-                                    @endphp
-                                    @foreach ($historyMovies as $historyItem)
-                                        @include('movie.parts.history_movie_item')
-                                    @endforeach
-                                </div>
-                            </div>
-                        </div> -->
                     </li>
                 @endif
                 @if (Auth::check())
                     {{-- 已登录 TODO: 登录后的UI交互 --}}
                     <li title="个人中心" dropdown-target=".user-center" dropdown-toggle="hover">
                         <a href="/movie/collection?type=like"><i class="iconfont icon-usercenter"></i></a>
-                        <!-- <div class="dropdown-box user-center open">
-                            <ul class="clearfix">
-                                <li><a class="item" href="/movie/collection?type=like">我的喜欢</a></li>
-                                <li><a class="item" href="/movie/collection?type=favorite">我的收藏</a></li>
-                                <li><a class="item logout">退出登录</a></li>
-                            </ul>
-                        </div> -->
                     </li>
                 @else
                     {{-- 未登录 TODO: 点击登录--}}
                     <li title="点击登录" data-toggle="modal" data-target="#login-modal">
-                        <a href="javascript:;">
+                        <a href="/login">
                             <i class="iconfont icon-account-fill"></i>
                         </a>
                     </li>
@@ -66,7 +45,3 @@
     </div>
     
 </header>
-
-<!-- @push('foot-scripts')
-    <script type="text/javascript" src="js/movie/header.js"></script>
-@endpush -->
