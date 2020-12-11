f5c2a79a9aaad5e0737520bc470a08a057bafbed
唐淑娟 <tangshujuan@haxifang.cn>
2020-12-11 18:25:12 +0800

    NHDY-282 fix footer &&search box

--- resources/views/layouts/movie/footer.blade.php
+++ resources/views/layouts/movie/footer.blade.php
@@ -1,40 +1,40 @@
 <footer class="app-footer">
-    <div class="app-footer_info">
-        <div class="app-footer__inner">
-            <dl class="app-footer__sns">
-                <dt class="app-footer__sns-label">Follow us</dt>
-                <dd class="app-footer__sns-cont">
-                    <ul class="app-footer__sns-lists">
-                        <li class="app-footer__sns-list">
-                            <a href="https://github.com/haxibiao" target="_blank">
-                                <svg class="app-footer__sns-icon app-footer__sns-icon--fb" t="1565228304869"
-                                    viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="1971"
-                                    xmlns:xlink="http://www.w3.org/1999/xlink">
-                                    <path
-                                        d="M512 42.666667A464.64 464.64 0 0 0 42.666667 502.186667 460.373333 460.373333 0 0 0 363.52 938.666667c23.466667 4.266667 32-9.813333 32-22.186667v-78.08c-130.56 27.733333-158.293333-61.44-158.293333-61.44a122.026667 122.026667 0 0 0-52.053334-67.413333c-42.666667-28.16 3.413333-27.733333 3.413334-27.733334a98.56 98.56 0 0 1 71.68 47.36 101.12 101.12 0 0 0 136.533333 37.973334 99.413333 99.413333 0 0 1 29.866667-61.44c-104.106667-11.52-213.333333-50.773333-213.333334-226.986667a177.066667 177.066667 0 0 1 47.36-124.16 161.28 161.28 0 0 1 4.693334-121.173333s39.68-12.373333 128 46.933333a455.68 455.68 0 0 1 234.666666 0c89.6-59.306667 128-46.933333 128-46.933333a161.28 161.28 0 0 1 4.693334 121.173333A177.066667 177.066667 0 0 1 810.666667 477.866667c0 176.64-110.08 215.466667-213.333334 226.986666a106.666667 106.666667 0 0 1 32 85.333334v125.866666c0 14.933333 8.533333 26.88 32 22.186667A460.8 460.8 0 0 0 981.333333 502.186667 464.64 464.64 0 0 0 512 42.666667"
-                                        fill="#231F20" p-id="1972"></path>
-                                </svg>
-                            </a>
-                        </li>
-                    </ul>
-                </dd>
-            </dl>
-            <ul class="app-footer__list">
-                <li class="app-footer__list-item">
-                    <a href="mailto:ivan@haxibiao.com?subject=Mail from Haxibiao.com Site" target="_blank">
-                        <span class="app-footer__list-txt">CONTACT US</span>
-                    </a>
-                </li>
-            </ul>
-            <p class="app-footer__copyright">© 2020 HASH-FUN</p>
-            <p class="ellipsis">
+    <div class="container app-footer_info">
+            <div class="app-footer__inner">
+                <dl class="app-footer__sns">
+                    <dt class="app-footer__sns-label">Follow us</dt>
+                    <dd class="app-footer__sns-cont">
+                        <ul class="app-footer__sns-lists">
+                            <li class="app-footer__sns-list">
+                                <a href="https://github.com/haxibiao" target="_blank">
+                                    <svg class="app-footer__sns-icon app-footer__sns-icon--fb" t="1565228304869"
+                                        viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="1971"
+                                        xmlns:xlink="http://www.w3.org/1999/xlink">
+                                        <path
+                                            d="M512 42.666667A464.64 464.64 0 0 0 42.666667 502.186667 460.373333 460.373333 0 0 0 363.52 938.666667c23.466667 4.266667 32-9.813333 32-22.186667v-78.08c-130.56 27.733333-158.293333-61.44-158.293333-61.44a122.026667 122.026667 0 0 0-52.053334-67.413333c-42.666667-28.16 3.413333-27.733333 3.413334-27.733334a98.56 98.56 0 0 1 71.68 47.36 101.12 101.12 0 0 0 136.533333 37.973334 99.413333 99.413333 0 0 1 29.866667-61.44c-104.106667-11.52-213.333333-50.773333-213.333334-226.986667a177.066667 177.066667 0 0 1 47.36-124.16 161.28 161.28 0 0 1 4.693334-121.173333s39.68-12.373333 128 46.933333a455.68 455.68 0 0 1 234.666666 0c89.6-59.306667 128-46.933333 128-46.933333a161.28 161.28 0 0 1 4.693334 121.173333A177.066667 177.066667 0 0 1 810.666667 477.866667c0 176.64-110.08 215.466667-213.333334 226.986666a106.666667 106.666667 0 0 1 32 85.333334v125.866666c0 14.933333 8.533333 26.88 32 22.186667A460.8 460.8 0 0 0 981.333333 502.186667 464.64 464.64 0 0 0 512 42.666667"
+                                            fill="#231F20" p-id="1972"></path>
+                                    </svg>
+                                </a>
+                            </li>
+                        </ul>
+                    </dd>
+                </dl>
+                <ul class="app-footer__list">
+                    <li class="app-footer__list-item">
+                        <a href="mailto:ivan@haxibiao.com?subject=Mail from Haxibiao.com Site" target="_blank">
+                            <span class="app-footer__list-txt">CONTACT US</span>
+                        </a>
+                    </li>
+                </ul>
+                <p class="app-footer__copyright">© 2020 HASH-FUN</p>
+                <p class="ellipsis">
 
-                本站所有图片和视频均来自互联网收集而来，版权归原创作者所有，本站只提供web页面服务，并不提供资源存储，也不参与录制，上传
-                <br />
-                若本站收录的内容无意侵犯了贵司版权，请发邮件至neihandianying@gmail.com, 我们会在3个工作日内删除侵权内容，谢谢。
+                    本站所有图片和视频均来自互联网收集而来，版权归原创作者所有，本站只提供web页面服务，并不提供资源存储，也不参与录制，上传
+                    <br />
+                    若本站收录的内容无意侵犯了贵司版权，请发邮件至neihandianying@gmail.com, 我们会在3个工作日内删除侵权内容，谢谢。
 
-            </p>
-        </div>
+                </p>
+            </div>
     </div>
 </footer>
 
