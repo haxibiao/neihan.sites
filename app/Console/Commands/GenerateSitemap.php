1033aaf0540995af9cb5d3ee560caaa6cf8efd5b
ivan <ivan@haxibiao.com>
2020-12-17 11:29:57 +0800

    更新sitemap的生成，不用www域名，不用.gz

--- app/Console/Commands/GenerateSitemap.php
+++ app/Console/Commands/GenerateSitemap.php
@@ -1,8 +1,6 @@
 <?php
 
-
 namespace App\Console\Commands;
-
 
 use App\Category;
 use Illuminate\Console\Command;
@@ -19,7 +17,7 @@ use wapmorgan\UnifiedArchive\UnifiedArchive;
  *  一个sitemap文件最多5w个链接,所有sitmap文件大家加起来不能超过50M
  *
  */
-class GenerateSitemap  extends Command
+class GenerateSitemap extends Command
 {
     protected $signature = 'sitemap:generate {--domain=}';
 
@@ -27,192 +25,198 @@ class GenerateSitemap  extends Command
 
     public function handle()
     {
-        $domain = $this->option('domain');
+        $domain             = $this->option('domain');
         $neihanSitesDomains = array_keys(neihan_sites_domains());
-        if($domain){
+        if ($domain) {
 //            if(!in_array($domain,$neihanSitesDomains)){
-//                return $this->error("输入的参数不合法");
-//            }
-            return self::generateSingleSitemap($domain);
+            //                return $this->error("输入的参数不合法");
+            //            }
+            return $this->generateSingleSitemap($domain);
         }
 
-        foreach ($neihanSitesDomains as $neihanSitesDomain){
-            self::generateSingleSitemap($neihanSitesDomain);
+        foreach ($neihanSitesDomains as $neihanSitesDomain) {
+            $this->generateSingleSitemap($neihanSitesDomain);
         }
     }
 
-    private function generateSingleSitemap($domain){
+    private function generateSingleSitemap($domain)
+    {
         $siteMapIndexUrls = array_merge(
-            self::generateMovies($domain),
-            self::generateIssues($domain),
-            self::generateCategory($domain),
-            self::generateArticles($domain),
-            self::generateVideos($domain)
+            $this->generateMovies($domain),
+            $this->generateIssues($domain),
+            $this->generateCategory($domain),
+            $this->generateArticles($domain),
+            $this->generateVideos($domain)
         );
         $siteMapIndex = SitemapIndex::create();
-        foreach ($siteMapIndexUrls as $siteMapIndexUrl){
-            $siteMapIndex->add(Sitemap::create('https://www.'.$domain.'/sitemap'.$siteMapIndexUrl)
-                ->setLastModificationDate(Carbon::yesterday()));
+        foreach ($siteMapIndexUrls as $siteMapIndexUrl) {
+            $siteMapIndex->add(Sitemap::create('https://' . $domain . '/sitemap' . $siteMapIndexUrl)
+                    ->setLastModificationDate(Carbon::yesterday()));
         }
-        $siteMapIndex->writeToDisk('public', 'sitemap/'.$domain.'/sitemap.xml');
-        return $this->info($domain."已经成功生成sitemap");
+        $siteMapIndex->writeToDisk('public', 'sitemap/' . $domain . '/sitemap.xml');
+        return $this->info($domain . "已经成功生成sitemap");
     }
 
-    private function generateMovies($domain){
+    private function generateMovies($domain)
+    {
         $siteMapIndexUrls = [];
-        $mi = 0;
+        $mi               = 0;
 
         DB::table('movies')->select(['id'])
-            ->orderBy('id','desc')->chunk(10000,function ($movies)use(&$mi,&$siteMapIndexUrls,$domain){
-                $fileName = 'movie_'.$mi.'.xml';
-                $gzFileName = $fileName.'.gz';
-                $relativePath = 'sitemap/'.$domain.'/'.$fileName;
-
-                $sitemapGenerator = SitemapGenerator::create($domain)
-                    ->getSitemap();
-                foreach ($movies as $movie){
-                    $sitemapGenerator->add(Url::create('https://www.'.$domain.'/movie/'.$movie->id)
-                        ->setLastModificationDate(Carbon::yesterday())
-                        ->setChangeFrequency(Url::CHANGE_FREQUENCY_DAILY)
-                        ->setPriority(0.8)
-                    );
-                }
-
-                $sitemapGenerator->writeToDisk('public', $relativePath);
-                $path = Storage::disk('public')->path($relativePath);
-                if(file_exists($path.'.gz')){
-                    unlink($path.'.gz');
-                }
-                UnifiedArchive::archiveFile($path, $path.'.gz');
-                $siteMapIndexUrls[] = '/'.$gzFileName;
-                $mi++;
-            });
-        return $siteMapIndexUrls;
-    }
-
-    private function generateIssues($domain){
-        $siteMapIndexUrls = [];
-        $qi = 0;
-        DB::table('issues')->select(['id'])
-            ->whereNull('deleted_at')
-            ->orderBy('id','desc')->chunk(10000,function ($questions)use(&$qi,&$siteMapIndexUrls,$domain){
-                $fileName = 'question_'.$qi.'.xml';
-                $gzFileName = $fileName.'.gz';
-                $relativePath = 'sitemap/'.$domain.'/'.$fileName;
-
-                $sitemapGenerator = SitemapGenerator::create($domain)
-                    ->getSitemap();
-                foreach ($questions as $question){
-                    $sitemapGenerator->add(Url::create('https://www.'.$domain.'/question/'.$question->id)
-                        ->setLastModificationDate(Carbon::yesterday())
-                        ->setChangeFrequency(Url::CHANGE_FREQUENCY_DAILY)
-                        ->setPriority(0.8)
-                    );
-                }
-                $sitemapGenerator->writeToDisk('public', $relativePath);
-                $path = Storage::disk('public')->path($relativePath);
-                if(file_exists($path.'.gz')){
-                    unlink($path.'.gz');
-                }
-                UnifiedArchive::archiveFile($path, $path.'.gz');
-                $siteMapIndexUrls[] = '/'.$gzFileName;
-                $qi++;
-            });
-        return $siteMapIndexUrls;
-    }
-
-    private function generateCategory($domain){
-        $siteMapIndexUrls = [];
-        $ci = 0;
-        Category::where('status',1)->where('count','>',0)->chunk(10000,function ($categories)use(&$ci,&$siteMapIndexUrls,$domain){
-            $fileName = 'category_'.$ci.'.xml';
-            $gzFileName = $fileName.'.gz';
-            $relativePath = 'sitemap/'.$domain.'/'.$fileName;
+            ->orderBy('id', 'desc')->chunk(10000, function ($movies) use (&$mi, &$siteMapIndexUrls, $domain) {
+            $fileName     = 'movie_' . $mi . '.xml';
+            $gzFileName   = $fileName . '.gz';
+            $relativePath = 'sitemap/' . $domain . '/' . $fileName;
 
             $sitemapGenerator = SitemapGenerator::create($domain)
                 ->getSitemap();
-            foreach ($categories as $category){
-                $sitemapGenerator->add(Url::create('https://www.'.$domain.'/category/'.$category->id)
-                    ->setLastModificationDate(Carbon::yesterday())
-                    ->setChangeFrequency(Url::CHANGE_FREQUENCY_DAILY)
-                    ->setPriority(0.8)
+            foreach ($movies as $movie) {
+                $sitemapGenerator->add(Url::create('https://' . $domain . '/movie/' . $movie->id)
+                        ->setLastModificationDate(Carbon::yesterday())
+                        ->setChangeFrequency(Url::CHANGE_FREQUENCY_DAILY)
+                        ->setPriority(0.8)
+                );
+            }
+
+            $sitemapGenerator->writeToDisk('public', $relativePath);
+            $path = Storage::disk('public')->path($relativePath);
+            if (file_exists($path . '.gz')) {
+                unlink($path . '.gz');
+            }
+            UnifiedArchive::archiveFile($path, $path . '.gz');
+            $siteMapIndexUrls[] = '/' . $fileName;
+            $mi++;
+        });
+        return $siteMapIndexUrls;
+    }
+
+    private function generateIssues($domain)
+    {
+        $siteMapIndexUrls = [];
+        $qi               = 0;
+        DB::table('issues')->select(['id'])
+            ->whereNull('deleted_at')
+            ->orderBy('id', 'desc')->chunk(10000, function ($questions) use (&$qi, &$siteMapIndexUrls, $domain) {
+            $fileName     = 'question_' . $qi . '.xml';
+            $gzFileName   = $fileName . '.gz';
+            $relativePath = 'sitemap/' . $domain . '/' . $fileName;
+
+            $sitemapGenerator = SitemapGenerator::create($domain)
+                ->getSitemap();
+            foreach ($questions as $question) {
+                $sitemapGenerator->add(Url::create('https://' . $domain . '/question/' . $question->id)
+                        ->setLastModificationDate(Carbon::yesterday())
+                        ->setChangeFrequency(Url::CHANGE_FREQUENCY_DAILY)
+                        ->setPriority(0.8)
                 );
             }
             $sitemapGenerator->writeToDisk('public', $relativePath);
             $path = Storage::disk('public')->path($relativePath);
-            if(file_exists($path.'.gz')){
-                unlink($path.'.gz');
+            if (file_exists($path . '.gz')) {
+                unlink($path . '.gz');
             }
-            UnifiedArchive::archiveFile($path, $path.'.gz');
+            UnifiedArchive::archiveFile($path, $path . '.gz');
+            $siteMapIndexUrls[] = '/' . $fileName;
+            $qi++;
+        });
+        return $siteMapIndexUrls;
+    }
 
-            $siteMapIndexUrls[] = '/'.$gzFileName;
+    private function generateCategory($domain)
+    {
+        $siteMapIndexUrls = [];
+        $ci               = 0;
+        Category::where('status', 1)->where('count', '>', 0)->chunk(10000, function ($categories) use (&$ci, &$siteMapIndexUrls, $domain) {
+            $fileName     = 'category_' . $ci . '.xml';
+            $gzFileName   = $fileName . '.gz';
+            $relativePath = 'sitemap/' . $domain . '/' . $fileName;
+
+            $sitemapGenerator = SitemapGenerator::create($domain)
+                ->getSitemap();
+            foreach ($categories as $category) {
+                $sitemapGenerator->add(Url::create('https://' . $domain . '/category/' . $category->id)
+                        ->setLastModificationDate(Carbon::yesterday())
+                        ->setChangeFrequency(Url::CHANGE_FREQUENCY_DAILY)
+                        ->setPriority(0.8)
+                );
+            }
+            $sitemapGenerator->writeToDisk('public', $relativePath);
+            $path = Storage::disk('public')->path($relativePath);
+            if (file_exists($path . '.gz')) {
+                unlink($path . '.gz');
+            }
+            UnifiedArchive::archiveFile($path, $path . '.gz');
+
+            $siteMapIndexUrls[] = '/' . $fileName;
             $ci++;
         });
         return $siteMapIndexUrls;
     }
 
-    private function generateArticles($domain){
+    private function generateArticles($domain)
+    {
         $siteMapIndexUrls = [];
-        $ai = 0;
+        $ai               = 0;
         DB::table('articles')->select(['id'])
-            ->where('status',1)
+            ->where('status', 1)
             ->whereNull('deleted_at')
-            ->whereIn('type',['article','diagrams'])->orderBy('id','desc')->chunk(10000,function ($articles)use(&$ai,&$siteMapIndexUrls,$domain){
+            ->whereIn('type', ['article', 'diagrams'])->orderBy('id', 'desc')->chunk(10000, function ($articles) use (&$ai, &$siteMapIndexUrls, $domain) {
 
-                $fileName = 'article_'.$ai.'.xml';
-                $gzFileName = $fileName.'.gz';
-                $relativePath = 'sitemap/'.$domain.'/'.$fileName;
+            $fileName     = 'article_' . $ai . '.xml';
+            $gzFileName   = $fileName . '.gz';
+            $relativePath = 'sitemap/' . $domain . '/' . $fileName;
 
-                $sitemapGenerator = SitemapGenerator::create($domain)
-                    ->getSitemap();
-                foreach ($articles as $article){
-                    $sitemapGenerator->add(Url::create('https://www.'.$domain.'/article/'.$article->id)
+            $sitemapGenerator = SitemapGenerator::create($domain)
+                ->getSitemap();
+            foreach ($articles as $article) {
+                $sitemapGenerator->add(Url::create('https://' . $domain . '/article/' . $article->id)
                         ->setLastModificationDate(Carbon::yesterday())
                         ->setChangeFrequency(Url::CHANGE_FREQUENCY_DAILY)
                         ->setPriority(0.8)
-                    );
-                }
-                $sitemapGenerator->writeToDisk('public', $relativePath);
-                $path = Storage::disk('public')->path($relativePath);
-                if(file_exists($path.'.gz')){
-                    unlink($path.'.gz');
-                }
-                UnifiedArchive::archiveFile($path, $path.'.gz');
-                $siteMapIndexUrls[] = '/'.$gzFileName;
-                $ai++;
+                );
+            }
+            $sitemapGenerator->writeToDisk('public', $relativePath);
+            $path = Storage::disk('public')->path($relativePath);
+            if (file_exists($path . '.gz')) {
+                unlink($path . '.gz');
+            }
+            UnifiedArchive::archiveFile($path, $path . '.gz');
+            $siteMapIndexUrls[] = '/' . $fileName;
+            $ai++;
 
-            });
+        });
         return $siteMapIndexUrls;
     }
 
-    private function generateVideos($domain){
+    private function generateVideos($domain)
+    {
         $siteMapIndexUrls = [];
-        $vi = 0;
+        $vi               = 0;
         DB::table('posts')->select(['video_id'])->whereNull('deleted_at')
             ->whereStatus(1)
-            ->orderBy('id','desc')->chunk(20000,function ($videos)use(&$vi,&$siteMapIndexUrls,$domain){
-                $fileName = 'video_'.$vi.'.xml';
-                $gzFileName = $fileName.'.gz';
-                $relativePath = 'sitemap/'.$domain.'/'.$fileName;
+            ->orderBy('id', 'desc')->chunk(20000, function ($videos) use (&$vi, &$siteMapIndexUrls, $domain) {
+            $fileName     = 'video_' . $vi . '.xml';
+            $gzFileName   = $fileName . '.gz';
+            $relativePath = 'sitemap/' . $domain . '/' . $fileName;
 
-                $sitemapGenerator = SitemapGenerator::create($domain)
-                    ->getSitemap();
-                foreach ($videos as $video){
-                    $sitemapGenerator->add(Url::create('https://www.'.$domain.'/video/'.$video->video_id)
+            $sitemapGenerator = SitemapGenerator::create($domain)
+                ->getSitemap();
+            foreach ($videos as $video) {
+                $sitemapGenerator->add(Url::create('https://' . $domain . '/video/' . $video->video_id)
                         ->setLastModificationDate(Carbon::yesterday())
                         ->setChangeFrequency(Url::CHANGE_FREQUENCY_DAILY)
                         ->setPriority(0.8)
-                    );
-                }
-                $sitemapGenerator->writeToDisk('public', $relativePath);
-                $path = Storage::disk('public')->path($relativePath);
-                if(file_exists($path.'.gz')){
-                    unlink($path.'.gz');
-                }
-                UnifiedArchive::archiveFile($path, $path.'.gz');
-                $siteMapIndexUrls[] = '/'.$gzFileName;
-                $vi++;
-            });
+                );
+            }
+            $sitemapGenerator->writeToDisk('public', $relativePath);
+            $path = Storage::disk('public')->path($relativePath);
+            if (file_exists($path . '.gz')) {
+                unlink($path . '.gz');
+            }
+            UnifiedArchive::archiveFile($path, $path . '.gz');
+            $siteMapIndexUrls[] = '/' . $fileName;
+            $vi++;
+        });
         return $siteMapIndexUrls;
     }
 }
