7ceb33d1602e013f3746043895c1029cfe039265
ivan <ivan@mac>
2020-12-26 17:35:20 +0800

    修复曾聪的资源token错误引起百度提交不成功

--- database/seeders/SiteSeeder.php
+++ database/seeders/SiteSeeder.php
@@ -22,7 +22,7 @@ class SiteSeeder extends Seeder
             $item->title        = '疯狂看美剧，快乐无极限';
             $item->keywords     = '在线美剧，在线韩剧，经典美剧，怀旧港剧，高清日剧';
             $item->description  = $name . ' ' . $domain . ' 是一个可以免费看全网影视大全的内涵电影网站';
-            $item->ziyuan_token = '=AWrriJorO5KMKgyj'; //曾聪的diudie
+            $item->ziyuan_token = 'AWrriJorO5KMKgyj'; //曾聪的diudie
             $item->save();
         }
     }
