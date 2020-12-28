19be06130a23b3cd0014c6f048d35f05dde9fcbd
ivan <ivan@mac>
2020-12-28 14:54:16 +0800

    修复 meta 支持站群模式

--- database/seeders/SiteSeeder.php
+++ database/seeders/SiteSeeder.php
@@ -22,8 +22,7 @@ class SiteSeeder extends Seeder
             // $item->keywords     = '在线美剧，在线韩剧，经典美剧，怀旧港剧，高清日剧';
             // $item->description  = $name . ' ' . $domain . ' 是一个可以免费看全网影视大全的内涵电影网站';
             // $item->ziyuan_token = 'AWrriJorO5KMKgyj'; //曾聪的diudie
-
-            $item->owner = '曾聪';
+            // $item->owner = '曾聪';
             $item->save();
         }
     }
