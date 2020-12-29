dcb1f5d9e66714033c8140cf0fb3cbad2b98ddc3
ivan <ivan@mac>
2020-12-29 22:23:49 +0800

    修复ops env 兼容jp002

--- ops/bash/env_beian.sh
+++ ops/bash/env_beian.sh
@@ -5,7 +5,7 @@ chmod -R 777 .env*
 git config core.filemode false
 
 echo "更新 env ..."
-php artisan set:env --db_host=localhost --db_database=neihan_sites
+php artisan set:env --db_host=ngz014 --db_database=neihan_sites
 
 echo "自动更新数据结构..."
 php artisan migrate
