62361d7939759bc604a96da655e6a742c4ded492
ivan <ivan@mac>
2020-12-29 22:57:48 +0800

    seo/baidu/include 检查备案和未备案结果分开

--- resources/views/seo/baidu_include.blade.php
+++ resources/views/seo/baidu_include.blade.php
@@ -3,30 +3,62 @@
 
 @section('content')
 <div class="container">
-  <div class="panel panel-default">
-    <div class="panel-heading">
-      <h3 class="panel-title">百度收录查询结果(缓存1天)</h3>
+  <div class="row">
+    <div class="col-md-6">
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h3 class="panel-title">备案站收录查询(缓存1天)</h3>
+        </div>
+    
+        <div class="panel-body">
+          <table class="table">
+            <tr>
+              <td>域名</td>
+              <td>已收录</td>
+              <td>索引量</td>
+            </tr>
+            @foreach($beian as $item)
+            <tr>
+              <td>{{ $item["url"] }}</td>
+              <td>{{ $item['收录']??0 > 0 ? '是':'' }}</td>
+              @if($item['收录']??0)
+                <td style="color:blue">{{ $item['收录']??0 }}</td>
+              @else
+                <td>{{ $item['收录']??0 }}</td>
+              @endif
+            </tr>
+            @endforeach
+          </table>
+        </div>
+      </div>
     </div>
-
-    <div class="panel-body">
-      <table class="table">
-        <tr>
-          <td>域名</td>
-          <td>已收录</td>
-          <td>索引量</td>
-        </tr>
-        @foreach($items as $item)
-        <tr>
-          <td>{{ $item["url"] }}</td>
-          <td>{{ $item['收录']??0 > 0 ? '是':'' }}</td>
-          @if($item['收录']??0)
-            <td style="color:blue">{{ $item['收录']??0 }}</td>
-          @else
-            <td>{{ $item['收录']??0 }}</td>
-          @endif
-        </tr>
-        @endforeach
-      </table>
+    <div class="col-md-6">
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h3 class="panel-title">内涵站收录查询(缓存1天)</h3>
+        </div>
+    
+        <div class="panel-body">
+          <table class="table">
+            <tr>
+              <td>域名</td>
+              <td>已收录</td>
+              <td>索引量</td>
+            </tr>
+            @foreach($neihan as $item)
+            <tr>
+              <td>{{ $item["url"] }}</td>
+              <td>{{ $item['收录']??0 > 0 ? '是':'' }}</td>
+              @if($item['收录']??0)
+                <td style="color:blue">{{ $item['收录']??0 }}</td>
+              @else
+                <td>{{ $item['收录']??0 }}</td>
+              @endif
+            </tr>
+            @endforeach
+          </table>
+        </div>
+      </div>
     </div>
   </div>
 </div>
