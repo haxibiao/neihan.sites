c273d48c7085a22f9fcfd47827cd2eb8fa0b4014
ivan <ivan@mac>
2021-01-01 16:49:45 +0800

    重构movie model controller 到media包

--- routes/web.php
+++ routes/web.php
@@ -33,17 +33,6 @@ Route::get('/app', 'IndexController@app');
 Route::get('/about-us', 'IndexController@aboutUs');
 Route::get('/trending', 'IndexController@trending');
 
-//电影
-Route::get('/movie/riju', 'MovieController@riju');
-Route::get('/movie/meiju', 'MovieController@meiju');
-Route::get('/movie/hanju', 'MovieController@hanju');
-Route::get('/movie/gangju', 'MovieController@gangju');
-Route::get('/movie/qita', 'MovieController@qita');
-Route::get('/movie/favorites', 'MovieController@favorites');
-
-Route::get('/movie/search', 'MovieController@search');
-Route::resource('/movie', 'MovieController');
-
 //问答
 // Route::resource('/question', 'IssueController');
 // Route::resource('/answer', 'ResolutionController');
