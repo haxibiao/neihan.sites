967e1546a6fc9f5cc316fa77a198c2a755ba1c31
ivan <ivan@mac>
2021-01-03 14:32:22 +0800

    修复站群影视页播放

--- config/media.php
+++ config/media.php
@@ -1,7 +1,18 @@
 <?php
 
 return [
-    'hook' => env('MEDIA_HOOK'), //'http://datizhuanqian.com/api/media/hook'
+    'hook'                           => env('MEDIA_HOOK'), //'http://datizhuanqian.com/api/media/hook'
+    'spider'                         => [
+        'enable'                              => env('MEDIA_SPIDER_ENABLE', true),
+        'user_daily_spider_parse_limit_count' => env('USER_DAILY_SPIDER_PARSE_LIMIT_COUNT', -1), // 用户每日可抓取最大次数 -1:无限制
+    ],
+
+    /**
+     * 电影模块配置
+     */
+    'movie'                          => [
+        'enable' => env('ENABLE_MOVIE', true),
+    ],
 
     /**
      * 是否统计视频的播放量
