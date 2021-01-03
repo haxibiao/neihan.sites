1085bc9da3eac1e3d48dccfb97753115402a5005
ivan <ivan@mac>
2021-01-03 14:37:27 +0800

    去掉对nhdy的依赖

--- app/Series.php
+++ app/Series.php
@@ -2,9 +2,9 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
+use Haxibiao\Media\Series as BaseSeries;
 
-class Series extends Model
+class Series extends BaseSeries
 {
-    public $connection = 'nhdy';
+
 }
