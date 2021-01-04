5e7f8ea899c1f50a7833658d8f766be5e845d2af
蔡志刚 <caizhigang@haxibiao.com>
2021-01-04 16:08:18 +0800

    NHY-317 added DOCTYPE to collect detail page

--- resources/views/share/collect.blade.php
+++ resources/views/share/collect.blade.php
@@ -1,5 +1,5 @@
+<!DOCTYPE html>
 <html>
-
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta content="width=device-width, initial-scale=0.8, maximum-scale=1.0, user-scalable=0" name="viewport">
@@ -7,12 +7,8 @@
 
     <link rel="stylesheet" type="text/css" href="/css/base.css" />
     <link rel="stylesheet" type="text/css" href="/css/collection.css" />
-
 </head>
-
-
 <body style="background-color: #161819;">
-
     <div id="war">
         <div id="top">
             <img src="{{$collection->logo}}">
