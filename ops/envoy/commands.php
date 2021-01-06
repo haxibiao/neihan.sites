a0c34dd4c2771e7dbb66baf6f04a992325976d40
ivan <ivan@mac>
2021-01-06 17:27:27 +0800

    修复网站电影详情页出错，清理过期ops代码

--- ops/envoy/commands.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// 临时命令
-
-$run_commands = <<<EOT
-
-php artisan fix:data articles
-
-EOT;
--- ops/envoy/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$domain = 'dianmoge.com';
-$db_server = 'gz010';
-$db_database = 'dianmoge';
-
-$domain_staging = 'staging.dianmoge.com';
-$db_server_staging = 'gz002';
-$db_database_staging = 'dianmoge_staging';
