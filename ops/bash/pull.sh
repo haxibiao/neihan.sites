c26c35d82757a1a83ae0f2382b298c5990990740
ivan <ivan@haxibiao.com>
2021-01-13 17:52:08 +0800

    临时清理过期的 base 包代码

--- ops/bash/pull.sh
+++ ops/bash/pull.sh
@@ -5,3 +5,6 @@ git submodule init && git submodule update && git pull --recurse-submodules
 
 echo "安装依赖..."
 composer install -o --ignore-platform-reqs
+
+echo "临时清理过期的 base 包代码"
+cd /data/www/neihan.sites && rm -rf ./packages/haxibiao/base
