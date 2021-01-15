ecb331a89417858bcc934f2348e44cd8b98d1ee0
ivan <ivan@haxibiao.com>
2021-01-15 15:47:51 +0800

    重构feedback到sns 并 sns:install

--- app/Tag.php
+++ app/Tag.php
@@ -2,8 +2,6 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Relations\MorphToMany;
-use Illuminate\Database\Eloquent\Relations\Relation;
 use Haxibiao\Content\Tag as BaseTag;
 
 class Tag extends BaseTag
@@ -16,59 +14,4 @@ class Tag extends BaseTag
         ],
     ];
 
-    // old relationship
-    public function creator()
-    {
-        return $this->belongsTo(\App\User::class);
-    }
-
-    public function articles():MorphToMany
-    {
-        return $this->taggable('App\Article');
-    }
-
-    public function videos():MorphToMany
-    {
-        return $this->taggable('App\Video');
-    }
-
-    public function posts():MorphToMany
-    {
-        return $this->taggable(\App\Post::class);
-    }
-
-    public function resolverPosts($rootValue, $args, $context, $resolveInfo){
-
-        $visibility = data_get($args,'visibility');
-        $order      = data_get($args,'order');
-        $user = getUser(false);
-
-        $qb = $rootValue->posts()->publish();
-
-        $qb->when( $visibility == 'self' , function ($q) use ($user){
-            $q->where('taggables.user_id',data_get($user,'id'));
-        });
-
-        $qb->when( $order == 'LATEST' , function ($q){
-            $q->orderByDesc('id');
-        });
-
-        return $qb;
-    }
-
-    public function resolveSearchTags($rootValue, $args, $context, $resolveInfo){
-        return Tag::search(data_get($args,'query'));
-    }
-
-    public function getCountPostsAttribute(){
-        return $this->count;
-    }
-
-    public function getCountViewsAttribute(){
-        $countViews = 0;
-        $this->posts()->with('video')->each(function ($post) use (&$countViews){
-            $countViews += data_get($post,'video.json.count_views',0);
-        });
-        return numberToReadable($countViews);
-    }
 }
