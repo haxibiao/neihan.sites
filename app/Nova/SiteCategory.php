cab47b27ebf55849f6b9b6b75758e7afb65730ba
蔡志刚 <caizhigang@haxibiao.com>
2021-01-19 09:31:11 +0800

    up...

--- app/Nova/SiteCategory.php
+++ app/Nova/SiteCategory.php
@@ -6,4 +6,5 @@ use Haxibiao\Cms\Nova\SiteCategory as NovaSiteCategory;
 
 class SiteCategory extends NovaSiteCategory
 {
+    public static $perPageOptions = [50, 100, 500, 1000];
 }
--- app/Nova/SitePost.php
+++ app/Nova/SitePost.php
@@ -6,4 +6,5 @@ use Haxibiao\Cms\Nova\SitePost as NovaSitePost;
 
 class SitePost extends NovaSitePost
 {
+    public static $perPageOptions = [50, 100, 500, 1000];
 }
