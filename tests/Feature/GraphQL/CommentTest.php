6470b2f6ff7aa84c5e3e117383b2c9edea26c98b
WangBin <WangBin@haxibiao.com>
2021-01-30 13:39:49 +0800

    HXB-98 UT: Comment GraphQL Test Case

--- tests/Feature/GraphQL/CommentTest.php
+++ tests/Feature/GraphQL/CommentTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature\GraphQL;
 use App\Article;
 use App\Comment;
 use App\Feedback;
+use App\Post;
 use App\User;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\Feature\GraphQL\TestCase;
@@ -17,7 +18,7 @@ class CommentTest extends GraphQLTestCase
     protected function setUp(): void
     {
         parent::setUp();
-        $this->user = factory(User::class)->create([
+        $this->user = User::factory()->make([
             'api_token' => str_random(60),
         ]);
     }
@@ -29,48 +30,49 @@ class CommentTest extends GraphQLTestCase
     /**
      * @group  testAddCommentMutation
      */
-    public function testAddCommentMutation()
+    public function atestCreateCommentMutation()
     {
         $token   = $this->user->api_token;
-        $query   = file_get_contents(__DIR__ . '/Comment/Mutation/addCommentMutation.gql');
+        $query   = file_get_contents(__DIR__ . '/Comment/Mutation/createCommentMutation.gql');
         $headers = [
             'Authorization' => 'Bearer ' . $token,
             'Accept'        => 'application/json',
         ];
         //情形一:评论动态
-        $comment = Article::where('type', 'post')->inRandomOrder()->first();
+        $comment = Post::where('status', 0)->inRandomOrder()->first();
 
         $variables = [
-            'commentable_type' => 'articles', //这个还是文章类型
-            'commentable_id'   => $comment->id,
-            'body'             => '评论动态',
+            'type' => 'posts', //这个还是文章类型
+            'id'   => $comment->id,
+            'content'             => '评论动态',
         ];
 
-        $this->runGuestGQL($query, $variables, $headers);
+        $this->runGQL($query, $variables, $headers);
 
         //情形二:评论评论
         $comment   = Comment::inRandomOrder()->first();
         $variables = [
-            'commentable_type' => 'comments',
-            'commentable_id'   => $comment->id,
-            'body'             => '评论评论',
+            'type' => 'comments',
+            'id'   => $comment->id,
+            'content'             => '评论评论',
         ];
 
-        $this->runGuestGQL($query, $variables, $headers);
+        $this->runGQL($query, $variables, $headers);
 
-        //情形二:评论评论
+        //情形二:评论反馈
         $comment   = Feedback::inRandomOrder()->first();
         $variables = [
-            'commentable_type' => 'feedbacks',
-            'commentable_id'   => $comment->id,
-            'body'             => '评论评论',
+            'type' => 'feedbacks',
+            'id'   => $comment->id,
+            'content'             => '评论反馈',
         ];
-        $this->runGuestGQL($query, $variables, $headers);
+        $this->runGQL($query, $variables, $headers);
     }
+
     /**
      * @group  testDeleteCommentMutation
      */
-    public function testDeleteCommentMutation()
+    public function atestDeleteCommentMutation()
     {
         $token   = $this->user->api_token;
         $query   = file_get_contents(__DIR__ . '/Comment/Mutation/deleteCommentMutation.gql');
@@ -84,38 +86,7 @@ class CommentTest extends GraphQLTestCase
             'id' => $comment->id,
         ];
 
-        $this->runGuestGQL($query, $variables, $headers);
-    }
-
-    /* --------------------------------------------------------------------- */
-    /* ------------------------------- Query ----------------------------- */
-    /* --------------------------------------------------------------------- */
-    /**
-     * @group  testCommentRepliesQuery
-     */
-    public function testCommentRepliesQuery()
-    {
-        $query     = file_get_contents(__DIR__ . '/Comment/Query/commentRepliesQuery.gql');
-        $comment   = Comment::where('commentable_type', 'comments')->first();
-        $variables = [
-            'id' => $comment->commentable_id,
-        ];
-        $this->runGuestGQL($query, $variables);
-    }
-    /**
-     * @group  testCommentsQuery
-     */
-    public function testCommentsQuery()
-    {
-        $query = file_get_contents(__DIR__ . '/Comment/Query/commentsQuery.gql');
-
-        $comment = Comment::where('commentable_type', 'articles')->first();
-
-        $variables = [
-            'commentable_id'   => $comment->commentable_id,
-            'commentable_type' => 'articles',
-        ];
-        $this->runGuestGQL($query, $variables);
+        $this->runGQL($query, $variables, $headers);
     }
 
     protected function tearDown(): void
